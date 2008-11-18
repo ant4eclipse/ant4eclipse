62edca6b908bbb4258642da232fa2664c7190bf3
nilshartmann <nilshartmann@d1cb4da4-e496-4248-86fa-ddd66e329890>
2008-11-18 21:01:47 +0000

    IN PROGRESS - issue AE-7: Rework net.sef.ant4eclipse.model.platform.team http://85.158.178.131:8080/browse/AE-7

---
+++ org.ant4eclipse.platform.test/src/net/sf/ant4eclipse/model/platform/team/projectset/internal/ProjectSetFileParserImplTest.java
@@ -0,0 +1,55 @@
+/**********************************************************************
+ * Copyright (c) 2005-2008 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package net.sf.ant4eclipse.model.platform.team.projectset.internal;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Properties;
+
+import net.sf.ant4eclipse.core.Ant4EclipseConfigurationProperties;
+import net.sf.ant4eclipse.core.logging.Ant4EclipseLogger;
+import net.sf.ant4eclipse.core.logging.DefaultAnt4EclipseLogger;
+import net.sf.ant4eclipse.model.platform.team.cvssupport.projectset.CvsTeamProjectSetFactory;
+import net.sf.ant4eclipse.model.platform.team.projectset.TeamProjectSetFactory;
+import net.sf.ant4eclipse.testframework.ServiceRegistryConfigurator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectSetFileParserImplTest {
+  
+  @Before
+  public void configure() {
+    Ant4EclipseConfigurationProperties.initialize();
+    
+    Properties properties = new Properties();
+    properties.put(Ant4EclipseLogger.class.getName(), DefaultAnt4EclipseLogger.class.getName());
+    ServiceRegistryConfigurator.configureServiceRegistry(properties);
+  }
+
+  @After
+  public void dispose() {
+    Ant4EclipseConfigurationProperties.dispose();
+  }
+  
+  @Test
+  public void test_Factories() {
+    TeamProjectSetFileParserImpl projectSetFileParserImpl = new TeamProjectSetFileParserImpl();
+    projectSetFileParserImpl.initialize();
+    TeamProjectSetFactory factoryForProvider = projectSetFileParserImpl.getFactoryForProvider("org.eclipse.team.cvs.core.cvsnature");
+    assertTrue(factoryForProvider instanceof CvsTeamProjectSetFactory);
+  }
+  
+  
+
+}
