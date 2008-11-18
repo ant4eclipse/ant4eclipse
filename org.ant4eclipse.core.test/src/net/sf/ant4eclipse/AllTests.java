983c3327d8fee56834d7d77cfd658519a421ef80
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2008-11-18 20:26:40 +0000

    IN PROGRESS - issue AE-2: Clean up net.sf.ant4eclipse.lang.util package http://www.ant4eclipse.net:8080/browse/AE-2

--- org.ant4eclipse.core.test/src/net/sf/ant4eclipse/AllTests.java
+++ org.ant4eclipse.core.test/src/net/sf/ant4eclipse/AllTests.java
@@ -5,6 +5,7 @@ import net.sf.ant4eclipse.core.AssertTest;
 import net.sf.ant4eclipse.core.dependencygraph.DependencyGraphTest;
 import net.sf.ant4eclipse.core.logging.A4ELogging_FailureTest;
 import net.sf.ant4eclipse.core.service.ServiceRegistryTest;
+import net.sf.ant4eclipse.core.util.JarUtilitiesTest;
 import net.sf.ant4eclipse.core.util.UtilitiesTest;
 import net.sf.ant4eclipse.core.xquery.XQueryHandlerTest;
 
@@ -12,7 +13,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { Ant4EclipseConfigurationPropertiesTest.class, AssertTest.class, XQueryHandlerTest.class,
-    UtilitiesTest.class, ServiceRegistryTest.class, A4ELogging_FailureTest.class, DependencyGraphTest.class })
+@Suite.SuiteClasses( { JarUtilitiesTest.class, Ant4EclipseConfigurationPropertiesTest.class, AssertTest.class,
+    XQueryHandlerTest.class, UtilitiesTest.class, ServiceRegistryTest.class, A4ELogging_FailureTest.class,
+    DependencyGraphTest.class })
 public class AllTests {
 }
