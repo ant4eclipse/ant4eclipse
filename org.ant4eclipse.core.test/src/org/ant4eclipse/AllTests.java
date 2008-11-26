9a747ed88877d6f261a36fa1822155898017522d
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2008-11-26 20:59:18 +0000

    + added PropertiesBasedServiceRegistryConfigurationTest

--- org.ant4eclipse.core.test/src/org/ant4eclipse/AllTests.java
+++ org.ant4eclipse.core.test/src/org/ant4eclipse/AllTests.java
@@ -3,6 +3,7 @@ package org.ant4eclipse;
 import org.ant4eclipse.core.AssertTest;
 import org.ant4eclipse.core.dependencygraph.DependencyGraphTest;
 import org.ant4eclipse.core.logging.A4ELogging_FailureTest;
+import org.ant4eclipse.core.service.PropertiesBasedServiceRegistryConfigurationTest;
 import org.ant4eclipse.core.service.ServiceRegistryTest;
 import org.ant4eclipse.core.util.JarUtilitiesTest;
 import org.ant4eclipse.core.util.UtilitiesTest;
@@ -12,6 +13,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { JarUtilitiesTest.class, AssertTest.class, XQueryHandlerTest.class, UtilitiesTest.class,
-    ServiceRegistryTest.class, A4ELogging_FailureTest.class, DependencyGraphTest.class })
+    ServiceRegistryTest.class, A4ELogging_FailureTest.class, PropertiesBasedServiceRegistryConfigurationTest.class,
+    DependencyGraphTest.class })
 public class AllTests {
 }
