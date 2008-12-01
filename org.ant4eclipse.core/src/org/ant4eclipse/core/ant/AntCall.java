b845ecf74c409fdc45b3aeaeb3139b8fe3a08c01
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2008-12-01 19:37:30 +0000

    ~ initial commit

---
+++ org.ant4eclipse.core/src/org/ant4eclipse/core/ant/AntCall.java
@@ -0,0 +1,124 @@
+/**********************************************************************
+ * Copyright (c) 2005-2008 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.core.ant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
+
+import org.ant4eclipse.core.Ant4EclipseConfigurator;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Ant;
+
+/**
+ * <p>
+ * 
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class AntCall extends Ant {
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link AntCall}.
+   * </p>
+   */
+  public AntCall() {
+    super();
+  }
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link AntCall}.
+   * </p>
+   * 
+   * @param owner
+   */
+  public AntCall(final Task owner) {
+    super(owner);
+  }
+
+  /**
+   * <p>
+   * Overrides the <code>execute()</code> method of the {@link Ant} super class. This method will return <b>without</b>
+   * an exception if the defined ant file or the defined target is not available.
+   * </p>
+   * 
+   * @see org.apache.tools.ant.taskdefs.Ant#execute()
+   */
+  @Override
+  public void execute() throws BuildException {
+
+    // configure ant4eclipse
+    Ant4EclipseConfigurator.configureAnt4Eclipse(getProject());
+
+    try {
+      // execute the super class
+      super.execute();
+    } catch (final BuildException buildException) {
+      // get the cause of the BuildException
+      final Throwable cause = getCause(buildException);
+
+      if (cause instanceof FileNotFoundException
+          && (cause.getMessage().indexOf(getAntFile()) != -1 || getAntFile() == null)) {
+        // ignore - FileNotFoundException is thrown because the given ant file doesn't exist.
+        // That's OK here...
+      } else if ((buildException.getMessage().indexOf("does not exist in the project") != -1)) {
+        buildException.printStackTrace();
+        // ignore
+      } else {
+        throw buildException;
+      }
+    }
+  }
+
+  /**
+   * <p>
+   * Gets the cause of the {@link BuildException}.
+   * </p>
+   * 
+   * @param throwable
+   *          the exception for which the cause should be returned
+   * @return the cause
+   */
+  private Throwable getCause(final Throwable throwable) {
+    if (throwable instanceof BuildException) {
+      return getCause(((BuildException) throwable).getException());
+    }
+    return throwable;
+  }
+
+  /**
+   * <p>
+   * Helper method that reads the (private) field <code>antfile</code> from the {@link Ant} class. If the private field
+   * can't be read, <code>null</code> will be returned.
+   * </p>
+   * 
+   * @return the content of the <code>antfile</code> field
+   */
+  private String getAntFile() {
+    try {
+      // retrieve the private field 'antFile' from the super class.
+      Field field = Ant.class.getDeclaredField("antFile");
+      // make it accessible
+      field.setAccessible(true);
+      // convert string to file
+      File file = new File((String) field.get(this));
+      return file.getAbsolutePath();
+    } catch (Exception e) {
+      // in case that an exception is thrown, null should be returned
+      return null;
+    }
+  }
+}
---
+++ org.ant4eclipse.core/src/org/ant4eclipse/core/ant/delegate/MacroExecutionDelegate.java
@@ -0,0 +1,161 @@
+/**********************************************************************
+ * Copyright (c) 2005-2008 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.core.ant.delegate;
+
+import java.lang.reflect.Field;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.ant4eclipse.core.Assert;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.MacroDef;
+import org.apache.tools.ant.taskdefs.MacroInstance;
+
+/**
+ */
+public class MacroExecutionDelegate extends AbstractAntDelegate {
+
+  /**
+   * @param component
+   */
+  public MacroExecutionDelegate(final Task task) {
+    super(task);
+  }
+
+  public MacroDef createMacroDef() {
+    final MacroDef macroDef = new MacroDef();
+    macroDef.setProject(getAntProject());
+    return macroDef;
+  }
+
+  public void executeMacroInstance(final MacroDef macroDef, final String prefix,
+      final Map<String, String> scopedProperties) {
+
+    Assert.notNull(macroDef);
+    Assert.nonEmpty(prefix);
+
+    // create MacroInstance
+    final MacroInstance instance = new MacroInstance();
+    instance.setProject(getAntProject());
+    instance.setOwningTarget(((Task) getProjectComponent()).getOwningTarget());
+    instance.setMacroDef(macroDef);
+
+    // set scopes properties
+    Iterator<Map.Entry<String, String>> iterator = scopedProperties.entrySet().iterator();
+    while (iterator.hasNext()) {
+      final Map.Entry<String, String> entry = iterator.next();
+      final String key = (prefix + "." + entry.getKey());
+      final String value = entry.getValue();
+      getAntProject().setProperty(key, value);
+    }
+
+    // execute macro instance
+    instance.execute();
+
+    // unset scopes properties
+    Iterator<String> keyIterator = scopedProperties.keySet().iterator();
+    while (keyIterator.hasNext()) {
+      final String key = (prefix + "." + keyIterator.next());
+      removeProperty(key);
+    }
+  }
+
+  private void removeProperty(final String name) {
+    Hashtable properties = null;
+    // Ant 1.5 stores properties in Project
+    try {
+      properties = (Hashtable) getValue(getAntProject(), "properties");
+      if (properties != null) {
+        properties.remove(name);
+      }
+    } catch (final Exception e) {
+      // ignore, could be Ant 1.6
+    }
+    try {
+      properties = (Hashtable) getValue(getAntProject(), "userProperties");
+      if (properties != null) {
+        properties.remove(name);
+      }
+    } catch (final Exception e) {
+      // ignore, could be Ant 1.6
+    }
+
+    // Ant 1.6 uses a PropertyHelper, can check for it by checking for a
+    // reference to "ant.PropertyHelper"
+    try {
+      final Object property_helper = getAntProject().getReference("ant.PropertyHelper");
+      if (property_helper != null) {
+        try {
+          properties = (Hashtable) getValue(property_helper, "properties");
+          if (properties != null) {
+            properties.remove(name);
+          }
+        } catch (final Exception e) {
+          // ignore
+        }
+        try {
+          properties = (Hashtable) getValue(property_helper, "userProperties");
+          if (properties != null) {
+            properties.remove(name);
+          }
+        } catch (final Exception e) {
+          // ignore
+        }
+      }
+    } catch (final Exception e) {
+      // ignore, could be Ant 1.5
+    }
+  }
+
+  /**
+   * Object rape: fondle the private parts of an object without it's permission.
+   * 
+   * @param instance
+   *          the object instance
+   * @param fieldName
+   *          the name of the field
+   * @return an object representing the value of the field
+   * @exception IllegalAccessException
+   *              foiled by the security manager
+   * @exception NoSuchFieldException
+   *              Darn, nothing to fondle
+   */
+  private Object getValue(final Object instance, final String fieldName) throws IllegalAccessException,
+      NoSuchFieldException {
+    final Field field = getField(instance.getClass(), fieldName);
+    field.setAccessible(true);
+    return field.get(instance);
+  }
+
+  /**
+   * Object rape: fondle the private parts of an object without it's permission.
+   * 
+   * @param thisClass
+   *          The class to rape.
+   * @param fieldName
+   *          The field to fondle
+   * @return The field value
+   * @exception NoSuchFieldException
+   *              Darn, nothing to fondle.
+   */
+  private Field getField(final Class thisClass, final String fieldName) throws NoSuchFieldException {
+    if (thisClass == null) {
+      throw new NoSuchFieldException("Invalid field : " + fieldName);
+    }
+    try {
+      return thisClass.getDeclaredField(fieldName);
+    } catch (final NoSuchFieldException e) {
+      return getField(thisClass.getSuperclass(), fieldName);
+    }
+  }
+}
