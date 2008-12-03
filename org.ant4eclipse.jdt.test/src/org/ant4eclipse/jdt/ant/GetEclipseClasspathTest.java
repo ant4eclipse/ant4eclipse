6e3ac2a275b225f669d6586316deea7f4da9c3ea
nilshartmann <nilshartmann@d1cb4da4-e496-4248-86fa-ddd66e329890>
2008-12-03 21:57:31 +0000

    ~ More tests

--- org.ant4eclipse.jdt.test/src/org/ant4eclipse/jdt/ant/GetEclipseClasspathTest.java
+++ org.ant4eclipse.jdt.test/src/org/ant4eclipse/jdt/ant/GetEclipseClasspathTest.java
@@ -11,25 +11,104 @@
  **********************************************************************/
 package org.ant4eclipse.jdt.ant;
 
+import java.io.File;
+
 import org.ant4eclipse.jdt.test.AbstractJDTBuildFileTest;
 import org.ant4eclipse.jdt.test.builder.JdtEclipseProjectBuilder;
 
 public class GetEclipseClasspathTest extends AbstractJDTBuildFileTest {
 
+  public final static String TEST_PATH_SEPARATOR = "#";
+
+  private File               _simpleProjectBinDir;
+
+  private File               _projectBBinDir;
+
   @Override
   public void setUp() throws Exception {
     super.setUp();
+    File simpleProjectDir = JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("simpleproject").createIn(
+        getWorkspaceDir());
+    _simpleProjectBinDir = new File(simpleProjectDir, "bin");
+
+    // projectB depends on simpleProject but doesn't re-export it
+    File projectbDir = JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("projectb").withSrcClasspathEntry(
+        "/simpleproject", false).createIn(getWorkspaceDir());
+    _projectBBinDir = new File(projectbDir, "bin");
 
     setupBuildFile("getEclipseClasspath.xml");
-    JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("simpleproject").createIn(getWorkspaceDir());
   }
 
   public void testSimple() throws Exception {
-    executeTarget("test-simple");
-    String classpath = getProject().getProperty("classpath");
-    System.out.println(classpath);
-    assertNotNull(classpath);
+    String classpath = executeTestTarget("simpleproject", false);
+    assertClasspath(classpath, _simpleProjectBinDir);
+  }
+
+  public void testSimple_TwoClassFolders() throws Exception {
+    File projectcDir = JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("projectc").withSrcClasspathEntry("gen-src",
+        "gen-classes", false).createIn(getWorkspaceDir());
+    String classpath = executeTestTarget("projectc", false);
+    assertClasspath(classpath, new File(projectcDir, "bin"), new File(projectcDir, "gen-classes"));
 
   }
 
+  public void test_WithProjectReferences() throws Exception {
+    String classpath = executeTestTarget("projectb", false);
+    assertClasspath(classpath, _projectBBinDir, _simpleProjectBinDir);
+  }
+
+  public void test_WithMultipleReferences() throws Exception {
+
+    // projectC references b. Since b doesn't reexport simple project, simpleproject will be invisible
+    File projectcDir = JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("projectc").withSrcClasspathEntry(
+        "/projectb", false).createIn(getWorkspaceDir());
+
+    String classpath = executeTestTarget("projectc", false);
+    File projectCBinDir = new File(projectcDir, "bin");
+    assertClasspath(classpath, projectCBinDir, _projectBBinDir);
+  }
+
+  public void test_WithMultipleReferencesRuntime() throws Exception {
+
+    // projectC references b. Since b doesn't reexport simple project, anyway: simpleproject will be visible
+    // since we ask for runtime classpath
+    File projectcDir = JdtEclipseProjectBuilder.getPreConfiguredJdtBuilder("projectc").withSrcClasspathEntry(
+        "/projectb", false).createIn(getWorkspaceDir());
+
+    String classpath = executeTestTarget("projectc", true);
+    File projectCBinDir = new File(projectcDir, "bin");
+    assertClasspath(classpath, projectCBinDir, _projectBBinDir, _simpleProjectBinDir);
+  }
+
+  /**
+   * Makes sure that the given classpath contains the expected entries
+   * 
+   * @param classpath
+   * @param expectedEntries
+   */
+  protected void assertClasspath(String classpath, File... expectedEntries) {
+    assertNotNull(classpath);
+    String[] classpathItems = classpath.split(TEST_PATH_SEPARATOR);
+    assertEquals(expectedEntries.length, classpathItems.length);
+
+    for (int i = 0; i < expectedEntries.length; i++) {
+      File expectedDir = expectedEntries[i];
+      File classpathItem = new File(classpathItems[i]);
+      assertEquals(String.format("Classpath-Item '%d' does not match. Expected: '%s' Actual: '%s'", i, expectedDir,
+          classpathItem), expectedDir, classpathItem);
+    }
+  }
+
+  protected String executeTestTarget(String projectName, boolean runtimeClasspath) throws Exception {
+    getProject().setProperty("projectName", projectName);
+    getProject().setProperty("runtimeClasspath", Boolean.toString(runtimeClasspath));
+    getProject().setProperty("pathSeparator", TEST_PATH_SEPARATOR);
+
+    executeTarget("getEclipseClasspath");
+
+    String classpath = getProject().getProperty("classpath");
+    System.out.println(classpath);
+    assertNotNull(classpath);
+    return classpath;
+  }
 }
