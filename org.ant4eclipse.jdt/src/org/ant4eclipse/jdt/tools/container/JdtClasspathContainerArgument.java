64c052f4459b0cb1ed7d81dc9b4fe65aa7911d82
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-02-02 22:59:02 +0000

    ~ added toString()

--- org.ant4eclipse.jdt/src/org/ant4eclipse/jdt/tools/container/JdtClasspathContainerArgument.java
+++ org.ant4eclipse.jdt/src/org/ant4eclipse/jdt/tools/container/JdtClasspathContainerArgument.java
@@ -38,4 +38,18 @@ public class JdtClasspathContainerArgument {
   public final void setValue(final String value) {
     this.value = value;
   }
+
+  /**
+   */
+  @Override
+  public String toString() {
+    final StringBuffer buffer = new StringBuffer();
+    buffer.append("[JdtClasspathContainerArgument:");
+    buffer.append(" key: ");
+    buffer.append(this.key);
+    buffer.append(" value: ");
+    buffer.append(this.value);
+    buffer.append("]");
+    return buffer.toString();
+  }
 }
