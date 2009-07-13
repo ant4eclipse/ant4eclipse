0c18f7d2a8175afbc380c7bc29b334026cbc7e66
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-07-13 18:24:46 +0000

    ~ support for local build ~ clean-up

--- org.ant4eclipse.build/build-ant4eclipse.sh
+++
@@ -1,2 +0,0 @@
-/usr/share/ant/bin/ant -lib $WORKSPACE/trunk/org.ant4eclipse.build/libs -lib $WORKSPACE/trunk/org.ant4eclipse.external/libs/ant-contrib -lib $WORKSPACE/trunk/org.ant4eclipse.external/libs/ejc -lib $WORKSPACE/trunk/org.ant4eclipse.external/libs/equinox -file $WORKSPACE/trunk/org.ant4eclipse.build/build-ant4eclipse.xml
-
