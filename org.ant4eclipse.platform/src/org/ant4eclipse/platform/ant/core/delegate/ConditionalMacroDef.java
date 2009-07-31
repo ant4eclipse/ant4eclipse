2b3ed454a810b1b21ce994db4063bca27639b492
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-07-31 15:00:17 +0000

    CLOSED - issue AE-84: Support for conditional execution in macro executions http://www.javakontor.org:8080/browse/AE-84

--- org.ant4eclipse.platform/src/org/ant4eclipse/platform/ant/core/delegate/ConditionalMacroDef.java
+++ org.ant4eclipse.platform/src/org/ant4eclipse/platform/ant/core/delegate/ConditionalMacroDef.java
@@ -5,108 +5,162 @@ import java.lang.reflect.Field;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.MacroDef;
 
+/**
+ * <p>
+ * Extends the {@link MacroDef} to support conditional execution.
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
 public class ConditionalMacroDef extends MacroDef {
 
+  /** the conditional nested sequential */
   private ConditionalNestedSequential _conditionalNestedSequential;
 
   /**
-   * @return
+   * <p>
+   * Returns the filter expression.
+   * </p>
+   * 
+   * @return the filter expression.
    */
   public String getFilter() {
     return this._conditionalNestedSequential.getFilter();
   }
 
   /**
-   * @return
+   * <p>
+   * Returns the 'if' condition.
+   * </p>
+   * 
+   * @return the 'if' condition.
    */
   public boolean isIf() {
     return this._conditionalNestedSequential.isIf();
   }
 
   /**
-   * @return
+   * <p>
+   * Returns the 'unless' condition.
+   * </p>
+   * 
+   * @return the 'unless' condition.
    */
   public boolean isUnless() {
     return this._conditionalNestedSequential.isUnless();
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public NestedSequential createSequential() {
     try {
+
+      // object rape - access the nestedSequential field...
       Field field = MacroDef.class.getDeclaredField("nestedSequential");
       field.setAccessible(true);
 
+      // copied from the original createSequential method
       if (field.get(this) != null) {
         throw new BuildException("Only one sequential allowed");
       }
+
+      // set the conditional nested sequential
       this._conditionalNestedSequential = new ConditionalNestedSequential();
       field.set(this, this._conditionalNestedSequential);
 
-      return (NestedSequential) field.get(this);
-    } catch (SecurityException e) {
-      // TODO Auto-generated catch block
+      // return the result
+      return this._conditionalNestedSequential;
+    } catch (Throwable e) {
+      // what should be do now!?
       e.printStackTrace();
-      throw new BuildException();
-    } catch (IllegalArgumentException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      throw new BuildException();
-    } catch (NoSuchFieldException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      throw new BuildException();
-    } catch (IllegalAccessException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-      throw new BuildException();
+      throw new BuildException("Internal Ant4Eclipse error...");
     }
   }
 
+  /**
+   * <p>
+   * Extends the {@link NestedSequential} to support conditional execution.
+   * </p>
+   * 
+   * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+   */
   public static class ConditionalNestedSequential extends MacroDef.NestedSequential {
 
-    /** - */
+    /** the sequential should only be executed if '_if == true' */
     private boolean _if     = true;
 
-    /** - */
+    /** the sequential should only be executed if '_unless == false' */
     private boolean _unless = false;
 
-    /** - */
+    /** a filter expression to filter the elements to execute a sequential for */
     private String  _filter = null;
 
     /**
-     * @return
+     * <p>
+     * Returns the filter expression.
+     * </p>
+     * 
+     * @return the filter expression.
      */
     public String getFilter() {
       return this._filter;
     }
 
     /**
+     * <p>
+     * Sets the filter expression.
+     * </p>
+     * 
      * @param filter
+     *          the filter expression.
      */
     public void setFilter(String filter) {
       this._filter = filter;
     }
 
     /**
-     * @return
+     * <p>
+     * Returns the 'if' condition.
+     * </p>
+     * 
+     * @return the 'if' condition.
      */
     public boolean isIf() {
       return this._if;
     }
 
-    public void setIf(boolean if1) {
-      this._if = if1;
+    /**
+     * <p>
+     * Sets the 'if' condition.
+     * </p>
+     * 
+     * @param aIf
+     *          the 'if' condition.
+     */
+    public void setIf(boolean aIf) {
+      this._if = aIf;
     }
 
     /**
-     * @return
+     * <p>
+     * Returns the 'unless' condition.
+     * </p>
+     * 
+     * @return the 'unless' condition.
      */
     public boolean isUnless() {
       return this._unless;
     }
 
     /**
-     * @param unless
+     * <p>
+     * Sets the 'unless' condition.
+     * </p>
+     * 
+     * @param the
+     *          'unless' condition.
      */
     public void setUnless(boolean unless) {
       this._unless = unless;
