d9c8783d419ffdbc0c1ee7767886f109f7d1c681
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-02 15:27:43 +0000

    - transferred the code from the class 'PythonProjectResolver' to the class 'PythonReferencedProjectResolverImpl'

--- org.ant4eclipse.pydt/src/org/ant4eclipse/pydt/internal/tools/PythonReferencedProjectResolverImpl.java
+++ org.ant4eclipse.pydt/src/org/ant4eclipse/pydt/internal/tools/PythonReferencedProjectResolverImpl.java
@@ -12,14 +12,24 @@
 package org.ant4eclipse.pydt.internal.tools;
 
 import org.ant4eclipse.core.logging.A4ELogging;
+import org.ant4eclipse.core.service.ServiceRegistry;
 import org.ant4eclipse.core.util.Utilities;
 
 import org.ant4eclipse.platform.model.resource.EclipseProject;
+import org.ant4eclipse.platform.model.resource.Workspace;
 import org.ant4eclipse.platform.tools.ReferencedProjectsResolver;
 
 import org.ant4eclipse.pydt.ant.usedargs.UsedProjectsArgumentComponent;
+import org.ant4eclipse.pydt.internal.model.project.PythonProjectRole;
+import org.ant4eclipse.pydt.model.RawPathEntry;
+import org.ant4eclipse.pydt.model.ReferenceKind;
+import org.ant4eclipse.pydt.model.ResolvedProjectEntry;
+import org.ant4eclipse.pydt.tools.PathEntryRegistry;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <p>
@@ -30,6 +40,21 @@ import java.util.List;
  * @author Daniel Kasmeroglu (Daniel.Kasmeroglu@Kasisoft.net)
  */
 public class PythonReferencedProjectResolverImpl implements ReferencedProjectsResolver {
+
+  private PathEntryRegistry             _registry;
+
+  private Workspace                     _workspace;
+
+  private UsedProjectsArgumentComponent _args;
+
+  /**
+   * Initialises this resolver implementation.
+   */
+  public PythonReferencedProjectResolverImpl() {
+    _registry = ServiceRegistry.instance().getService(PathEntryRegistry.class);
+    _workspace = null;
+    _args = null;
+  }
 
   /**
    * {@inheritDoc}
@@ -42,13 +67,14 @@ public class PythonReferencedProjectResolverImpl implements ReferencedProjectsRe
    * {@inheritDoc}
    */
   public List<EclipseProject> resolveReferencedProjects(EclipseProject project, List<Object> additionalElements) {
-    final UsedProjectsArgumentComponent args = getArgs(additionalElements);
-    if (args.isExport() && PythonUtilities.isPyDevProject(project)) {
+    _args = getArgs(additionalElements);
+    if (_args.isExport() && PythonUtilities.isPyDevProject(project)) {
       A4ELogging.warn("The mode 'exported' is treated as 'all' for a PyDev project.");
     }
-    final PythonProjectResolver resolver = new PythonProjectResolver(project.getWorkspace());
-    resolver.setArgs(args);
-    return resolver.resolve(project);
+    _workspace = project.getWorkspace();
+    List<EclipseProject> result = new ArrayList<EclipseProject>();
+    resolve(result, loadEntries(project));
+    return result;
   }
 
   /**
@@ -72,6 +98,90 @@ public class PythonReferencedProjectResolverImpl implements ReferencedProjectsRe
       }
     }
     return UsedProjectsArgumentComponent.DEFAULT;
+  }
+
+  /**
+   * Returns a list of raw path entries from the supplied project.
+   * 
+   * @param project
+   *          The project which raw path entries have to be returned. Not <code>null</code>.
+   * 
+   * @return A list of raw path entries. Not <code>null</code>.
+   */
+  private List<RawPathEntry> loadEntries(final EclipseProject project) {
+    final List<RawPathEntry> result = new ArrayList<RawPathEntry>();
+    final PythonProjectRole role = (PythonProjectRole) project.getRole(PythonProjectRole.class);
+    final RawPathEntry[] entries = role.getRawPathEntries(ReferenceKind.Project);
+    for (RawPathEntry entry : entries) {
+      result.add(entry);
+    }
+    return result;
+  }
+
+  /**
+   * Resolving process which tries to lookup projects depending on the raw path entries needed to be processed.
+   * 
+   * @param receiver
+   *          A list used to collect the resolved process. Not <code>null</code>.
+   * @param entries
+   *          The raw path entries needed to be processed. Not <code>null</code>.
+   */
+  private void resolve(final List<EclipseProject> receiver, final List<RawPathEntry> entries) {
+
+    final Set<RawPathEntry> followed = new HashSet<RawPathEntry>();
+    while (!entries.isEmpty()) {
+
+      // get the first entry
+      final RawPathEntry entry = entries.remove(0);
+      if (!_registry.isResolved(entry)) {
+        // until now it has not been resolved, so resolve and register it
+        _registry.registerResolvedPathEntry(entry, resolveRawPathEntry(entry));
+      }
+      // access the resolved path entry
+      final ResolvedProjectEntry resolved = (ResolvedProjectEntry) _registry.getResolvedPathEntry(entry);
+      // fetch the EclipseProject instance from the workspace
+      final EclipseProject refproject = _workspace.getProject(resolved.getProjectname());
+      if (!receiver.contains(refproject)) {
+        // only add the project if that hasn't been done yet
+        receiver.add(refproject);
+      }
+
+      // indirection handling: only enter if it has not been followed yet
+      if ((!followed.contains(entry)) && (!_args.isDirect())) {
+
+        if (_args.isAll()) {
+          // this mode overrides any path setting
+          followed.add(entry);
+          entries.addAll(loadEntries(refproject));
+        } else if (_args.isExport() && entry.isExported()) {
+          // just follow exported entries
+          followed.add(entry);
+          entries.addAll(loadEntries(refproject));
+        }
+
+      }
+
+    }
+
+  }
+
+  /**
+   * Creates a resolved project entry from the supplied raw entry.
+   * 
+   * @param entry
+   *          A raw entry used to refer to a project. Not <code>null</code>.
+   * 
+   * @return The resolved project entry equivalent. Not <code>null</code>.
+   */
+  private ResolvedProjectEntry resolveRawPathEntry(RawPathEntry entry) {
+    String value = entry.getValue();
+    if ((value.charAt(0) != '/') || (value.length() == 1)) {
+      /** @todo [02-Aug-2009:KASI] We need to cause an exception here. */
+      A4ELogging.warn("The raw projectname '%s' does not start conform to the required format '/' <identifier> !",
+          value);
+      return null;
+    }
+    return new ResolvedProjectEntry(value.substring(1));
   }
 
 } /* ENDCLASS */
