dc3f6942a63eb059a34a3c533957f68234a9f0ba
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-07 10:27:24 +0000

    ~ the lister now generates a string representation directly parseable by the 'Version' class

--- org.ant4eclipse.pydt/resources/org/ant4eclipse/pydt/lister.py
+++ org.ant4eclipse.pydt/resources/org/ant4eclipse/pydt/lister.py
@@ -2,8 +2,8 @@ import sys
 
 if __name__ == "__main__":
     print "ANT4ECLIPSE-BEGIN"
-    print sys.version_info
+    print "_".join([".".join([repr(x) for x in sys.version_info[:-2]]),sys.version_info[3]])
     for i in sys.path:
-        print i
+        print "[" + i + "]"
     print "ANT4ECLIPSE-END"
 
