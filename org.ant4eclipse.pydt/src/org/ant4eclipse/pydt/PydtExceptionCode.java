c4bb74c1d0fd27c49e1438bd10f834d463322d6e
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-07 12:23:33 +0000

    ~ collection of python specific failure codes

---
+++ org.ant4eclipse.pydt/src/org/ant4eclipse/pydt/PydtExceptionCode.java
@@ -0,0 +1,34 @@
+/**********************************************************************
+ * Copyright (c) 2005-2008 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.pydt;
+
+import org.ant4eclipse.core.nls.NLS;
+import org.ant4eclipse.core.nls.NLSMessage;
+
+/**
+ * ExceptionCodes for Pydt tools.
+ * 
+ * @author Daniel Kasmeroglu (Daniel.Kasmeroglu@Kasisoft.net)
+ */
+public class PydtExceptionCode {
+
+  @NLSMessage("Project '%s' must have a python project role (either PyDev or Python DLTK).")
+  public static String NO_PYTHON_PROJECT_ROLE;
+
+  @NLSMessage("The python runtime with the id '%s' has not been registered (see 'pythonContainer').")
+  public static String UNKNOWN_PYTHON_RUNTIME;
+
+  static {
+    NLS.initialize(PydtExceptionCode.class);
+  }
+
+} /* ENDCLASS */
