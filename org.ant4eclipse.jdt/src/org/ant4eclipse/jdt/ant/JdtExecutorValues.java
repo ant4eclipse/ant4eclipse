dc45b264adb05ba137ecb886cb652f0248676975
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-07 08:45:25 +0000

    ~ removed redundant modifiers

--- org.ant4eclipse.jdt/src/org/ant4eclipse/jdt/ant/JdtExecutorValues.java
+++ org.ant4eclipse.jdt/src/org/ant4eclipse/jdt/ant/JdtExecutorValues.java
@@ -3,53 +3,53 @@ package org.ant4eclipse.jdt.ant;
 public interface JdtExecutorValues {
 
   /** reference id for the CompilerArgument */
-  static final String COMPILER_ARGS                       = "compiler.args";
+  String COMPILER_ARGS                       = "compiler.args";
 
   /** */
-  static final String CLASSPATH_RELATIVE_RUNTIME_PATH     = "classpath.relative.runtime.path";
+  String CLASSPATH_RELATIVE_RUNTIME_PATH     = "classpath.relative.runtime.path";
 
-  static final String CLASSPATH_ABSOLUTE_RUNTIME_PATH     = "classpath.absolute.runtime.path";
+  String CLASSPATH_ABSOLUTE_RUNTIME_PATH     = "classpath.absolute.runtime.path";
 
-  static final String CLASSPATH_RELATIVE_COMPILETIME_PATH = "classpath.relative.compiletime.path";
+  String CLASSPATH_RELATIVE_COMPILETIME_PATH = "classpath.relative.compiletime.path";
 
-  static final String CLASSPATH_ABSOLUTE_COMPILETIME_PATH = "classpath.absolute.compiletime.path";
+  String CLASSPATH_ABSOLUTE_COMPILETIME_PATH = "classpath.absolute.compiletime.path";
 
-  static final String BOOT_CLASSPATH_PATH                 = "boot.classpath.path";
+  String BOOT_CLASSPATH_PATH                 = "boot.classpath.path";
 
-  static final String DEFAULT_OUTPUT_DIRECTORY_PATH       = "default.output.directory.path";
+  String DEFAULT_OUTPUT_DIRECTORY_PATH       = "default.output.directory.path";
 
-  static final String SOURCE_DIRECTORIES_PATH             = "source.directories.path";
+  String SOURCE_DIRECTORIES_PATH             = "source.directories.path";
 
-  static final String OUTPUT_DIRECTORIES_PATH             = "output.directories.path";
+  String OUTPUT_DIRECTORIES_PATH             = "output.directories.path";
 
-  static final String OUTPUT_DIRECTORY_PATH               = "output.directory.path";
+  String OUTPUT_DIRECTORY_PATH               = "output.directory.path";
 
-  static final String SOURCE_DIRECTORY_PATH               = "source.directory.path";
+  String SOURCE_DIRECTORY_PATH               = "source.directory.path";
 
-  static final String CLASSPATH_RELATIVE_RUNTIME          = "classpath.relative.runtime";
+  String CLASSPATH_RELATIVE_RUNTIME          = "classpath.relative.runtime";
 
-  static final String CLASSPATH_ABSOLUTE_RUNTIME          = "classpath.absolute.runtime";
+  String CLASSPATH_ABSOLUTE_RUNTIME          = "classpath.absolute.runtime";
 
-  static final String CLASSPATH_RELATIVE_COMPILETIME      = "classpath.relative.compiletime";
+  String CLASSPATH_RELATIVE_COMPILETIME      = "classpath.relative.compiletime";
 
-  static final String CLASSPATH_ABSOLUTE_COMPILETIME      = "classpath.absolute.compiletime";
+  String CLASSPATH_ABSOLUTE_COMPILETIME      = "classpath.absolute.compiletime";
 
-  static final String BOOT_CLASSPATH                      = "boot.classpath";
+  String BOOT_CLASSPATH                      = "boot.classpath";
 
-  static final String DEFAULT_OUTPUT_DIRECTORY            = "default.output.directory";
+  String DEFAULT_OUTPUT_DIRECTORY            = "default.output.directory";
 
-  static final String DEFAULT_OUTPUT_DIRECTORY_NAME       = "default.output.directory.name";
+  String DEFAULT_OUTPUT_DIRECTORY_NAME       = "default.output.directory.name";
 
-  static final String SOURCE_DIRECTORIES                  = "source.directories";
+  String SOURCE_DIRECTORIES                  = "source.directories";
 
-  static final String OUTPUT_DIRECTORIES                  = "output.directories";
+  String OUTPUT_DIRECTORIES                  = "output.directories";
 
-  static final String OUTPUT_DIRECTORY                    = "output.directory";
+  String OUTPUT_DIRECTORY                    = "output.directory";
 
-  static final String OUTPUT_DIRECTORY_NAME               = "output.directory.name";
+  String OUTPUT_DIRECTORY_NAME               = "output.directory.name";
 
-  static final String SOURCE_DIRECTORY                    = "source.directory";
+  String SOURCE_DIRECTORY                    = "source.directory";
 
-  static final String SOURCE_DIRECTORY_NAME               = "source.directory.name";
+  String SOURCE_DIRECTORY_NAME               = "source.directory.name";
 
 }
