faa1df6bfe739720c252c30fe2b6bf655df6c6f9
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-10 11:41:07 +0000

    + encoding

---
+++ org.ant4eclipse.platform/src/org/ant4eclipse/platform/internal/model/resource/encoding/ResourcesPreferences.java
@@ -0,0 +1,16 @@
+package org.ant4eclipse.platform.internal.model.resource.encoding;
+
+import java.io.File;
+
+/**
+ * <p>
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class ResourcesPreferences {
+
+  public String getEncoding(File file) {
+    return null;
+  }
+}
---
+++ org.ant4eclipse.platform/src/org/ant4eclipse/platform/internal/model/resource/encoding/ResourcesPreferencesParser.java
@@ -0,0 +1,9 @@
+package org.ant4eclipse.platform.internal.model.resource.encoding;
+
+public class ResourcesPreferencesParser {
+
+  private static final String RESOURCES_PREFERENCES_FILE = ".settings/org.eclipse.core.resources.prefs";
+
+  private static final String ENCODING_PREFIX            = "encoding/";
+
+}
