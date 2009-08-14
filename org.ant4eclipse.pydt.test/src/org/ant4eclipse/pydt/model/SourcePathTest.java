7d8654b0702fa283dea9b256e2cca375d8c3aaae
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-14 12:21:03 +0000

    + commitment of an initial testcase

---
+++ org.ant4eclipse.pydt.test/src/org/ant4eclipse/pydt/model/SourcePathTest.java
@@ -0,0 +1,26 @@
+/**********************************************************************
+ * Copyright (c) 2005-2009 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.pydt.model;
+
+import org.ant4eclipse.testframework.ConfigurableAnt4EclipseTestCase;
+import org.junit.Assert;
+
+/**
+ * @author Daniel Kasmeroglu (Daniel.Kasmeroglu@Kasisoft.net)
+ */
+public class SourcePathTest extends ConfigurableAnt4EclipseTestCase {
+
+  public void testDummy() {
+    Assert.fail("Oops");
+  }
+
+} /* ENDCLASS */
