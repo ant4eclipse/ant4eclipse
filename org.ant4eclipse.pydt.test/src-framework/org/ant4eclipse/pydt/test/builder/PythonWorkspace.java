fec78a55358a20b6bf682c2a54488451cb8452a8
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-14 14:52:03 +0000

    + initial class used to provide a workspace for python

---
+++ org.ant4eclipse.pydt.test/src-framework/org/ant4eclipse/pydt/test/builder/PythonWorkspace.java
@@ -0,0 +1,76 @@
+/**********************************************************************
+ * Copyright (c) 2005-2009 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.pydt.test.builder;
+
+import org.ant4eclipse.core.util.Utilities;
+
+import org.ant4eclipse.platform.test.builder.EclipseProjectBuilder;
+
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.File;
+
+/**
+ * Helper class used to represent a single workspace.
+ * 
+ * @author Daniel Kasmeroglu (Daniel.Kasmeroglu@Kasisoft.net)
+ */
+public class PythonWorkspace {
+
+  private File _workspacedir;
+
+  /**
+   * Initialises this workspace.
+   */
+  public PythonWorkspace() {
+    _workspacedir = Utilities.createTempDir();
+  }
+
+  /**
+   * Prepares the workspace so the tests can be executed.
+   */
+  @Before
+  public void setup() {
+  }
+
+  /**
+   * Cleansup the workspace so temporarily generated code will be removed.
+   */
+  @After
+  public void cleanup() {
+  }
+
+  /**
+   * Adds the supplied project to the workspace.
+   * 
+   * @param projectbuilder
+   *          The project builder used to add a project. Not <code>null</code>.
+   */
+  public void addProject(final EclipseProjectBuilder projectbuilder) {
+    projectbuilder.createIn(_workspacedir);
+  }
+
+  /**
+   * Removes the supplied project from the workspace.
+   * 
+   * @param projectbuilder
+   *          The project builder used to remove a project. Not <code>null</code>.
+   */
+  public void removeProject(final EclipseProjectBuilder projectbuilder) {
+    final File dir = new File(_workspacedir, projectbuilder.getProjectName());
+    if (dir.isDirectory()) {
+      Utilities.delete(dir);
+    }
+  }
+
+} /* ENDCLASS */
