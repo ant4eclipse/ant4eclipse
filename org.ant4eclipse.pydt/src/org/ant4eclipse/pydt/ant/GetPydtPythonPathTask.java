0cc3ab62aaee4cb029234d0acde75d8a3cd51dff
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-08-18 13:03:58 +0000

    ~ added an attribute 'ignoreruntime' allowing to resolve the path without the runtime

--- org.ant4eclipse.pydt/src/org/ant4eclipse/pydt/ant/GetPydtPythonPathTask.java
+++ org.ant4eclipse.pydt/src/org/ant4eclipse/pydt/ant/GetPydtPythonPathTask.java
@@ -11,14 +11,15 @@
  **********************************************************************/
 package org.ant4eclipse.pydt.ant;
 
-import org.ant4eclipse.platform.model.resource.EclipseProject;
-
 import org.ant4eclipse.pydt.internal.model.project.PythonProjectRole;
 import org.ant4eclipse.pydt.internal.tools.PythonResolver;
 import org.ant4eclipse.pydt.model.RawPathEntry;
+import org.ant4eclipse.pydt.model.ReferenceKind;
 import org.ant4eclipse.pydt.model.ResolvedPathEntry;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Basic task used to access the source path of a python project.
@@ -27,16 +28,50 @@ import java.io.File;
  */
 public class GetPydtPythonPathTask extends AbstractPydtGetProjectPathTask {
 
+  private boolean _ignoreruntime = false;
+
+  /**
+   * Enables/disables the support for the runtime.
+   * 
+   * @param ignoreruntime
+   *          <code>true</code> <=> The python runtime shall be ignored.
+   */
+  public void setIgnoreruntime(final boolean ignoreruntime) {
+    _ignoreruntime = ignoreruntime;
+  }
+
   /**
    * {@inheritDoc}
    */
   protected File[] resolvePath() {
     final PythonProjectRole role = (PythonProjectRole) getEclipseProject().getRole(PythonProjectRole.class);
     final PythonResolver resolver = new PythonResolver();
-    final RawPathEntry[] entries = role.getRawPathEntries();
+    RawPathEntry[] entries = role.getRawPathEntries();
+    if (_ignoreruntime) {
+      // the runtimes shall be ignored, so don't resolve them
+      entries = removeRuntimeEntries(entries);
+    }
     final ResolvedPathEntry[] resolved = resolver.resolve(entries);
-    final File[] result = resolver.expand(resolved, getEclipseProject(), EclipseProject.PathStyle.ABSOLUTE);
+    final File[] result = resolver.expand(resolved, getEclipseProject(), getPathStyle());
     return result;
+  }
+
+  /**
+   * Removes the runtime entries so they will not be used for the resolving process.
+   * 
+   * @param entries
+   *          A list of entries needed to be filtered. Not <code>null</code>.
+   * 
+   * @return A filtered list of entries not containing the {@link ReferenceKind#Runtime}.
+   */
+  private RawPathEntry[] removeRuntimeEntries(final RawPathEntry[] entries) {
+    List<RawPathEntry> list = new ArrayList<RawPathEntry>();
+    for (int i = 0; i < entries.length; i++) {
+      if (entries[i].getKind() != ReferenceKind.Runtime) {
+        list.add(entries[i]);
+      }
+    }
+    return list.toArray(new RawPathEntry[list.size()]);
   }
 
 } /* ENDCLASS */
