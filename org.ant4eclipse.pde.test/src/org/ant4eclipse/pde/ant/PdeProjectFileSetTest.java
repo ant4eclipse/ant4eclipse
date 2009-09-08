004d78e53a7daf5bd26b1946f39b8419e42b2a6a
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-09-08 18:32:29 +0000

    ~ fixed test case

--- org.ant4eclipse.pde.test/src/org/ant4eclipse/pde/ant/PdeProjectFileSetTest.java
+++ org.ant4eclipse.pde.test/src/org/ant4eclipse/pde/ant/PdeProjectFileSetTest.java
@@ -3,48 +3,35 @@ package org.ant4eclipse.pde.ant;
 import org.ant4eclipse.pde.test.builder.PdeProjectBuilder;
 import org.ant4eclipse.testframework.AbstractTestDirectoryBasedBuildFileTest;
 
-public class PdeProjectFileSetTest extends
-		AbstractTestDirectoryBasedBuildFileTest {
+public class PdeProjectFileSetTest extends AbstractTestDirectoryBasedBuildFileTest {
 
-	public void setUp() {
-		super.setUp();
-		configureProject("src/org/ant4eclipse/pde/ant/PdeProjectFileSetTest.xml");
-	}
+  public void setUp() {
+    super.setUp();
+    configureProject("src/org/ant4eclipse/pde/ant/PdeProjectFileSetTest.xml");
+  }
 
-	@Override
-	protected void tearDown() {
-	}
+  public void testPdeProjectFileSet_simple() {
 
-	public void testPdeProjectFileSet_simple() {
+    // set some ant properties
+    getProject().setProperty("workspace", getTestDirectory().getRootDir().getAbsolutePath());
+    getProject().setProperty("projectname", "simpleproject1");
+    getProject().setProperty("projectname", "simpleproject1");
+    getProject().setProperty("bin.includes", "META-INF/,.,my/src/");
+    getProject().setProperty("bin.excludes", "my/src/de/gerd-wuetherich/test/Test.java,**/Test3.java");
 
-		// set some ant properties
-		getProject().setProperty("workspace",
-				getTestDirectory().getRootDir().getAbsolutePath());
-		getProject().setProperty("projectname", "simpleproject1");
-		getProject().setProperty("projectname", "simpleproject1");
-		getProject().setProperty("bin.includes", "META-INF/,.,my/src/");
-		getProject().setProperty("bin.excludes",
-				"my/src/de/gerd-wuetherich/test/Test.java,**/Test3.java");
+    // create simple project
+    PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder.getPreConfiguredPdeProjectBuilder("simpleproject1");
+    pdeProjectBuilder.withSourceClass("@dot", "de.gerd-wuetherich.test.Gerd").finishClass();
+    pdeProjectBuilder.withSourceClass("my/src", "de.gerd-wuetherich.test.Test").finishClass();
+    pdeProjectBuilder.withSourceClass("my/src", "de.gerd-wuetherich.test.Test2").finishClass();
+    pdeProjectBuilder.withSourceClass("my/src", "de.gerd-wuetherich.test.Test3").finishClass();
+    pdeProjectBuilder.createIn(getTestDirectoryRootDir());
 
-		// create simple project
-		PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder
-				.getPreConfiguredPdeProjectBuilder("simpleproject1");
-		pdeProjectBuilder.withSourceClass("@dot",
-				"de.gerd-wuetherich.test.Gerd").finishClass();
-		pdeProjectBuilder.withSourceClass("my/src",
-				"de.gerd-wuetherich.test.Test").finishClass();
-		pdeProjectBuilder.withSourceClass("my/src",
-				"de.gerd-wuetherich.test.Test2").finishClass();
-		pdeProjectBuilder.withSourceClass("my/src",
-				"de.gerd-wuetherich.test.Test3").finishClass();
-		pdeProjectBuilder.createIn(getTestDirectoryRootDir());
+    // expected log
+    String rootPath = getTestDirectoryRootDir().getAbsolutePath();
+    String expectedLog = normalize("%s\\simpleproject1\\META-INF\\MANIFEST.MF  %s\\simpleproject1\\@dot\\de\\gerd-wuetherich\\test\\Gerd.java  %s\\simpleproject1\\my\\src\\de\\gerd-wuetherich\\test\\Test2.java");
 
-		// expected log
-		String rootPath = getTestDirectoryRootDir().getAbsolutePath();
-		String expectedLog = "%s\\simpleproject1\\META-INF\\MANIFEST.MF  %s\\simpleproject1\\@dot\\de\\gerd-wuetherich\\test\\Gerd.java  %s\\simpleproject1\\my\\src\\de\\gerd-wuetherich\\test\\Test2.java";
-
-		// execute test
-		expectLog("testMultipleDirectoriesFileSet", String.format(expectedLog,
-				rootPath, rootPath, rootPath));
-	}
+    // execute test
+    expectLog("testMultipleDirectoriesFileSet", String.format(expectedLog, rootPath, rootPath, rootPath));
+  }
 }
