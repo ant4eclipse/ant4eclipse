ad7fca104567196ef944a7b3ca2c7fd189f75a74
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-09-10 13:53:55 +0000

    IN PROGRESS - issue AE-100: Implement a PDE project / build.properties checker http://www.javakontor.org:8080/jira/browse/AE-100

--- org.ant4eclipse.pde.test/src/org/ant4eclipse/pde/ant/ExecutePluginLibraryTaskTest.java
+++ org.ant4eclipse.pde.test/src/org/ant4eclipse/pde/ant/ExecutePluginLibraryTaskTest.java
@@ -4,76 +4,95 @@ import org.ant4eclipse.pde.test.builder.PdeProjectBuilder;
 
 import org.ant4eclipse.testframework.AbstractTestDirectoryBasedBuildFileTest;
 
-public class ExecutePluginLibraryTaskTest extends AbstractTestDirectoryBasedBuildFileTest {
+public class ExecutePluginLibraryTaskTest extends
+		AbstractTestDirectoryBasedBuildFileTest {
 
-  public void setUp() {
-    super.setUp();
-    
-    configureProject("src/org/ant4eclipse/pde/ant/ExecutePluginLibraryTaskTest.xml");
-  }
+	public void setUp() {
+		super.setUp();
 
-  public void testPdeProjectFileSet_simple() {
+		configureProject("src/org/ant4eclipse/pde/ant/ExecutePluginLibraryTaskTest.xml");
+	}
 
-    // set some ant properties
-    getProject().setProperty("workspaceDirectory", getTestDirectory().getRootDir().getAbsolutePath());
-    getProject().setProperty("project.name", "simpleproject1");
-    getProject().setProperty("library.name", ".");
+	public void testPdeProjectFileSet_simple() {
 
-    // create simple project
-    PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder.getPreConfiguredPdeProjectBuilder("simpleproject1");
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test").finishClass();
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test2").finishClass();
-    pdeProjectBuilder.withBuildProperties().withLibrary(".").withSource("src").withSource("resource").withOutput("bin")
-        .finishLibrary();
-    pdeProjectBuilder.createIn(getTestDirectoryRootDir());
+		// set some ant properties
+		getProject().setProperty("workspaceDirectory",
+				getTestDirectory().getRootDir().getAbsolutePath());
+		getProject().setProperty("project.name", "simpleproject1");
+		getProject().setProperty("library.name", ".");
 
-    // expected log
-    String rootPath = getTestDirectoryRootDir().getAbsolutePath();
-    String expectedLog = normalize("- %1s\\simpleproject1\\bin -- bin -- %1s\\simpleproject1\\src -- src -- %1s\\simpleproject1\\resource -- resource -");
+		// create simple project
+		PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder
+				.getPreConfiguredPdeProjectBuilder("simpleproject1");
+		pdeProjectBuilder
+				.withSourceClass("src", "de.gerd-wuetherich.test.Test")
+				.finishClass();
+		pdeProjectBuilder.withSourceClass("src",
+				"de.gerd-wuetherich.test.Test2").finishClass();
+		pdeProjectBuilder.withBuildProperties().withLibrary(".").withSource(
+				"src").withSource("resource").withOutput("bin").finishLibrary();
+		pdeProjectBuilder.createIn(getTestDirectoryRootDir());
 
-    // execute test
-    expectLog("testExecutePluginLibrary", String.format(expectedLog, rootPath, rootPath, rootPath));
-  }
+		// expected log
+		String rootPath = getTestDirectoryRootDir().getAbsolutePath();
+		String expectedLog = normalize("- %1s\\simpleproject1\\bin -- bin -- %1s\\simpleproject1\\src -- src -- %1s\\simpleproject1\\resource -- resource -");
 
-  public void testPdeProjectFileSet_libraryDoesNotExist() {
+		// execute test
+		expectLog("testExecutePluginLibrary", String.format(expectedLog,
+				rootPath, rootPath, rootPath));
+	}
 
-    // set some ant properties
-    getProject().setProperty("workspaceDirectory", getTestDirectory().getRootDir().getAbsolutePath());
-    getProject().setProperty("project.name", "simpleproject1");
-    getProject().setProperty("library.name", "hurz");
+	public void testPdeProjectFileSet_libraryDoesNotExist() {
 
-    // create simple project
-    PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder.getPreConfiguredPdeProjectBuilder("simpleproject1");
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test").finishClass();
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test2").finishClass();
-    pdeProjectBuilder.withBuildProperties().withLibrary(".").withSource("src").withSource("resource").withOutput("bin")
-        .finishLibrary();
-    pdeProjectBuilder.createIn(getTestDirectoryRootDir());
+		// set some ant properties
+		getProject().setProperty("workspaceDirectory",
+				getTestDirectory().getRootDir().getAbsolutePath());
+		getProject().setProperty("project.name", "simpleproject1");
+		getProject().setProperty("library.name", "hurz");
 
-    // execute test
-    expectSpecificBuildException("testExecutePluginLibrary", "libraryDoesNotExist", "org.ant4eclipse.core.exception.Ant4EclipseException: Library 'hurz' doesn't exist in project 'simpleproject1'.");
-  }
-  
-  public void testPdeProjectFileSet_libraryWithoutSource() {
+		// create simple project
+		PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder
+				.getPreConfiguredPdeProjectBuilder("simpleproject1");
+		pdeProjectBuilder
+				.withSourceClass("src", "de.gerd-wuetherich.test.Test")
+				.finishClass();
+		pdeProjectBuilder.withSourceClass("src",
+				"de.gerd-wuetherich.test.Test2").finishClass();
+		pdeProjectBuilder.withBuildProperties().withLibrary(".").withSource(
+				"src").withSource("resource").withOutput("bin").finishLibrary();
+		pdeProjectBuilder.createIn(getTestDirectoryRootDir());
 
-    // set some ant properties
-    getProject().setProperty("workspaceDirectory", getTestDirectory().getRootDir().getAbsolutePath());
-    getProject().setProperty("project.name", "simpleproject1");
-    getProject().setProperty("library.name", ".");
+		// execute test
+		expectSpecificBuildException(
+				"testExecutePluginLibrary",
+				"libraryDoesNotExist",
+				"org.ant4eclipse.core.exception.Ant4EclipseException: Library 'hurz' doesn't exist in project 'simpleproject1'.");
+	}
 
-    // create simple project
-    PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder.getPreConfiguredPdeProjectBuilder("simpleproject1");
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test").finishClass();
-    pdeProjectBuilder.withSourceClass("src", "de.gerd-wuetherich.test.Test2").finishClass();
-    pdeProjectBuilder.withBuildProperties().withLibrary(".").withOutput("bin")
-        .finishLibrary();
-    pdeProjectBuilder.createIn(getTestDirectoryRootDir());
+	public void testPdeProjectFileSet_libraryWithoutSource() {
 
-    // expected log
-    String rootPath = getTestDirectoryRootDir().getAbsolutePath();
-    String expectedLog = normalize("- %1s\\simpleproject1\\bin -- bin -- %1s\\simpleproject1\\src -- src -- %1s\\simpleproject1\\resource -- resource -");
+		// set some ant properties
+		getProject().setProperty("workspaceDirectory",
+				getTestDirectory().getRootDir().getAbsolutePath());
+		getProject().setProperty("project.name", "simpleproject1");
+		getProject().setProperty("library.name", ".");
 
-    // execute test
-    expectLog("testExecutePluginLibrary", String.format(expectedLog, rootPath, rootPath, rootPath));
-  }
+		// create simple project
+		PdeProjectBuilder pdeProjectBuilder = PdeProjectBuilder
+				.getPreConfiguredPdeProjectBuilder("simpleproject1");
+		pdeProjectBuilder
+				.withSourceClass("src", "de.gerd-wuetherich.test.Test")
+				.finishClass();
+		pdeProjectBuilder.withSourceClass("src",
+				"de.gerd-wuetherich.test.Test2").finishClass();
+		pdeProjectBuilder.withBuildProperties().withLibrary(".").withOutput(
+				"bin").finishLibrary();
+		pdeProjectBuilder.createIn(getTestDirectoryRootDir());
+
+		// execute test
+		expectSpecificBuildException(
+				"testExecutePluginLibrary",
+				"libraryDoesNotExist",
+				"org.ant4eclipse.core.exception.Ant4EclipseException: Library '.' doesn't exist in project 'simpleproject1'.");
+	}
 }
