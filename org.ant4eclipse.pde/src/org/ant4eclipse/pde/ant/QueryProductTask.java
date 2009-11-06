3a74d3bd213d145ebaca5daffae8386836956325
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-11-06 10:52:13 +0000

    ~ extended the query functionalities

--- org.ant4eclipse.pde/src/org/ant4eclipse/pde/ant/QueryProductTask.java
+++ org.ant4eclipse.pde/src/org/ant4eclipse/pde/ant/QueryProductTask.java
@@ -18,7 +18,12 @@ import org.ant4eclipse.core.util.Utilities;
 import org.ant4eclipse.pde.model.product.ProductDefinition;
 import org.ant4eclipse.pde.model.product.ProductDefinitionParser;
 
+import org.ant4eclipse.platform.ant.core.delegate.WorkspaceDelegate;
+import org.ant4eclipse.platform.model.resource.EclipseProject;
+import org.ant4eclipse.platform.model.resource.EclipseProject.PathStyle;
+
 import org.apache.tools.ant.BuildException;
+import org.osgi.framework.Version;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -40,6 +45,27 @@ public class QueryProductTask extends AbstractAnt4EclipseTask {
   public static enum Query {
 
     /** - */
+    basedonfeatures,
+
+    /** - */
+    configini,
+
+    /** - */
+    programargs,
+
+    /** - */
+    vmargs,
+
+    /** - */
+    application,
+
+    /** - */
+    version,
+
+    /** - */
+    launchername,
+
+    /** - */
     plugins,
 
     /** - */
@@ -50,17 +76,89 @@ public class QueryProductTask extends AbstractAnt4EclipseTask {
 
   }
 
-  /** - */
-  private String _property;
+  /**
+   * Selection of the operating system currently used to query the product configuration.
+   */
+  public static enum Os {
+
+    /** - */
+    linux,
+
+    /** - */
+    solaris,
+
+    /** - */
+    macosx,
+
+    /** - */
+    win32;
+
+  }
 
   /** - */
-  private File   _product;
+  private String            _property;
 
   /** - */
-  private Query  _query;
+  private File              _product;
 
   /** - */
-  private String _delimiter = ",";
+  private Query             _query;
+
+  /** - */
+  private String            _delimiter;
+
+  /** - */
+  private Os                _os;
+
+  /** - */
+  private WorkspaceDelegate _workspacedelegate;
+
+  /** - */
+  private boolean           _defaultisempty;
+
+  /**
+   * Initialises this task with default values.
+   */
+  public QueryProductTask() {
+    super();
+    this._workspacedelegate = new WorkspaceDelegate(this);
+    this._os = Os.win32;
+    this._delimiter = ",";
+    this._query = null;
+    this._product = null;
+    this._property = null;
+    this._defaultisempty = true;
+  }
+
+  /**
+   * Enables/disables the property setting for the case no value is available.
+   * 
+   * @param newdefaultisempty
+   *          <code>true</code> <=> Enables the property setting (empty value).
+   */
+  public void setDefaultIsEmpty(boolean newdefaultisempty) {
+    this._defaultisempty = newdefaultisempty;
+  }
+
+  /**
+   * Sets the workspace directory. Only necessary for queries trying to access filesystem information.
+   * 
+   * @param workspace
+   *          The workspace directory. Not <code>null</code>.
+   */
+  public final void setWorkspaceDirectory(File workspace) {
+    this._workspacedelegate.setWorkspaceDirectory(workspace);
+  }
+
+  /**
+   * Changes the current os used for the querying of the product configuration.
+   * 
+   * @param newos
+   *          The new os used for the querying of the product configuration. Not <code>null</code>.
+   */
+  public void setOs(Os newos) {
+    this._os = newos;
+  }
 
   /**
    * Changes the query used to access the product information.
@@ -121,6 +219,26 @@ public class QueryProductTask extends AbstractAnt4EclipseTask {
       throw new ExtendedBuildException("The attribute 'query' has to be set to one of the following values: %s",
           Utilities.listToString(Query.values(), null));
     }
+    if (this._query == Query.configini) {
+      this._workspacedelegate.requireWorkspaceDirectorySet();
+    }
+  }
+
+  /**
+   * Loads the product definition.
+   * 
+   * @return The loaded product definition. Not <code>null</code>.
+   */
+  private ProductDefinition loadProductDefinition() {
+    InputStream instream = null;
+    try {
+      instream = new FileInputStream(this._product);
+      return ProductDefinitionParser.parseProductDefinition(instream);
+    } catch (IOException ex) {
+      throw new BuildException(ex);
+    } finally {
+      Utilities.close(instream);
+    }
   }
 
   /**
@@ -128,16 +246,7 @@ public class QueryProductTask extends AbstractAnt4EclipseTask {
    */
   @Override
   protected void doExecute() {
-    ProductDefinition productdef = null;
-    InputStream instream = null;
-    try {
-      instream = new FileInputStream(this._product);
-      productdef = ProductDefinitionParser.parseProductDefinition(instream);
-    } catch (IOException ex) {
-      throw new BuildException(ex);
-    } finally {
-      Utilities.close(instream);
-    }
+    ProductDefinition productdef = loadProductDefinition();
     String value = null;
     switch (this._query) {
     case plugins:
@@ -146,10 +255,75 @@ public class QueryProductTask extends AbstractAnt4EclipseTask {
     case fragments:
       value = Utilities.listToString(productdef.getFragmentIds(), this._delimiter);
       break;
+    case features:
+      value = Utilities.listToString(productdef.getFeatureIds(), this._delimiter);
+      break;
+    case launchername:
+      value = productdef.getLaunchername();
+      break;
+    case application:
+      value = productdef.getApplication();
+      break;
+    case version:
+      Version version = productdef.getVersion();
+      if (version != null) {
+        value = version.toString();
+      }
+      break;
+    case programargs:
+      value = getArgs(productdef.getProgramArgs(), productdef.getProgramArgs(ProductDefinition.Os.valueOf(this._os
+          .name())));
+      break;
+    case vmargs:
+      value = getArgs(productdef.getVmArgs(), productdef.getVmArgs(ProductDefinition.Os.valueOf(this._os.name())));
+      break;
+    case configini:
+      String configini = productdef.getConfigIni(ProductDefinition.Os.valueOf(this._os.name()));
+      if (configini != null) {
+        int idx = configini.indexOf('/', 1);
+        String projectname = configini.substring(1, idx);
+        String path = configini.substring(idx + 1);
+        EclipseProject project = this._workspacedelegate.getWorkspace().getProject(projectname);
+        value = project.getChild(path, PathStyle.ABSOLUTE).getAbsolutePath();
+      }
+      break;
+    case basedonfeatures:
+      value = String.valueOf(productdef.isBasedOnFeatures());
+      break;
     default:
       throw new ExtendedBuildException("The query type '%s' is currently not implemented.", this._query);
     }
-    getProject().setProperty(this._property, value);
+    if (value != null) {
+      getProject().setProperty(this._property, value);
+    } else {
+      if (this._defaultisempty) {
+        getProject().setProperty(this._property, "");
+      }
+    }
+  }
+
+  /**
+   * Concatenates the supplied arguments/argumentlists.
+   * 
+   * @param args
+   *          The arguments/argumentlists that have to be concatenated.
+   * 
+   * @return The resulting argumentlist. Maybe <code>null</code>.
+   */
+  private String getArgs(String... args) {
+    if (args == null) {
+      return null;
+    }
+    StringBuffer buffer = new StringBuffer();
+    for (String arg : args) {
+      if (arg != null) {
+        if (buffer.length() > 0) {
+          buffer.append(' ');
+        }
+        buffer.append(arg);
+      }
+    }
+    return Utilities.cleanup(buffer.toString());
   }
 
 } /* ENDCLASS */
