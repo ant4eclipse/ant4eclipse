3d1c3a78af45d49f242453fec8c356b547c7ab2a
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-11-26 14:54:58 +0000

    ~ fix: the property 'buildPlugin.newBundleVersion' has been set within the macro. this means that it has only been set once since properties cannot be altered which would cause invalid version values for subsequent calls (later calls would still use the version of the first built plugin). the property has now been removed at the end.

--- org.ant4eclipse.pde/src/org/ant4eclipse/pde/ant/ExecuteProductTask.java
+++ org.ant4eclipse.pde/src/org/ant4eclipse/pde/ant/ExecuteProductTask.java
@@ -12,29 +12,38 @@
 package org.ant4eclipse.pde.ant;
 
 import org.ant4eclipse.core.ant.ExtendedBuildException;
+import org.ant4eclipse.core.ant.FileListHelper;
 import org.ant4eclipse.core.exception.Ant4EclipseException;
 import org.ant4eclipse.core.util.StringMap;
 import org.ant4eclipse.core.util.Utilities;
 
 import org.ant4eclipse.pde.PdeExceptionCode;
+import org.ant4eclipse.pde.model.pluginproject.BundleSource;
 import org.ant4eclipse.pde.model.product.ProductDefinition;
 import org.ant4eclipse.pde.model.product.ProductDefinitionParser;
 import org.ant4eclipse.pde.model.product.ProductOs;
+import org.ant4eclipse.pde.tools.TargetPlatform;
+import org.ant4eclipse.pde.tools.TargetPlatformConfiguration;
+import org.ant4eclipse.pde.tools.TargetPlatformRegistry;
 
 import org.ant4eclipse.platform.PlatformExceptionCode;
 import org.ant4eclipse.platform.ant.core.MacroExecutionValues;
 import org.ant4eclipse.platform.ant.core.ScopedMacroDefinition;
 import org.ant4eclipse.platform.ant.core.delegate.MacroExecutionValuesProvider;
 import org.ant4eclipse.platform.ant.core.task.AbstractExecuteProjectTask;
+import org.ant4eclipse.platform.model.resource.EclipseProject;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.MacroDef;
+import org.eclipse.osgi.service.resolver.BundleDescription;
 import org.osgi.framework.Version;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * <p>
@@ -53,6 +62,8 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
     ForProduct,
     /** - */
     ForEachFeature,
+    // /** - */
+    // ForEachTargetFeature,
     /** - */
     ForEachPlugin
   }
@@ -93,9 +104,16 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
   /** - */
   private static final String         PROP_PLUGINISSOURCE  = "plugin.isSource";
 
-  // configini,
+  /** - */
+  private static final String         PROP_PLUGINFILE      = "plugin.file";
+
+  /** - */
+  private static final String         PROP_PLUGINFILELIST  = "plugin.filelist";
+
+  /** - */
+  private static final String         PROP_CONFIGINI       = "configini";
+
   // programargs,
-  // vmargs,
   // wsplugins,
   // wsfragments,
   // wsfeatures,
@@ -210,18 +228,29 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
 
     ProductDefinition productdef = loadProductDefinition();
 
+    // create a new target platform configuration
+    TargetPlatformConfiguration configuration = new TargetPlatformConfiguration();
+    configuration.setPreferProjects(true);
+
+    // fetch the target platform
+    TargetPlatform targetplatform = TargetPlatformRegistry.Helper.getRegistry().getInstance(getWorkspace(),
+        getTargetPlatformId(), configuration);
+
     // execute scoped macro definitions
     for (ScopedMacroDefinition<String> scopedmacro : getScopedMacroDefinitions()) {
       Scope scope = Scope.valueOf(scopedmacro.getScope());
       switch (scope) {
       case ForProduct:
-        executeForProduct(productdef, scopedmacro.getMacroDef());
+        executeForProduct(productdef, scopedmacro.getMacroDef(), targetplatform);
         break;
       case ForEachFeature:
-        executeForEachFeature(productdef, scopedmacro.getMacroDef());
+        executeForEachFeature(productdef, scopedmacro.getMacroDef(), targetplatform);
         break;
+      // case ForEachTargetFeature:
+      // executeForEachTargetFeature(productdef, scopedmacro.getMacroDef(), targetplatform);
+      // break;
       case ForEachPlugin:
-        executeForEachPlugin(productdef, scopedmacro.getMacroDef());
+        executeForEachPlugin(productdef, scopedmacro.getMacroDef(), targetplatform);
         break;
       }
     }
@@ -235,26 +264,25 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
    *          The productdefinition to be used. Not <code>null</code>.
    * @param macrodef
    *          The macro constituting the scope. Not <code>null</code>.
+   * @param targetplatform
+   *          The TargetPlatform used to resolve the bundles against. Not <code>null</code>.
    */
-  private void executeForEachPlugin(ProductDefinition productdef, MacroDef macrodef) {
+  private void executeForEachPlugin(ProductDefinition productdef, MacroDef macrodef, TargetPlatform targetplatform) {
 
     StringMap properties = new StringMap();
+    Map<String, Object> references = new Hashtable<String, Object>();
     contributeForAll(properties, productdef);
 
     String[] pluginids = productdef.getPluginIds();
     for (String pluginid : pluginids) {
-      if (getWorkspace().hasProject(pluginid)) {
-        contributeForPlugin(properties, productdef, pluginid);
-        executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties));
-      }
+      contributeForPlugin(properties, references, productdef, pluginid, targetplatform);
+      executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties, references));
     }
 
     String[] fragmentids = productdef.getFragmentIds();
     for (String fragmentid : fragmentids) {
-      if (getWorkspace().hasProject(fragmentid)) {
-        contributeForPlugin(properties, productdef, fragmentid);
-        executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties));
-      }
+      contributeForPlugin(properties, references, productdef, fragmentid, targetplatform);
+      executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties, references));
     }
 
   }
@@ -266,8 +294,10 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
    *          The productdefinition to be used. Not <code>null</code>.
    * @param macrodef
    *          The macro constituting the scope. Not <code>null</code>.
+   * @param targetplatform
+   *          The TargetPlatform used to resolve the bundles against. Not <code>null</code>.
    */
-  private void executeForEachFeature(ProductDefinition productdef, MacroDef macrodef) {
+  private void executeForEachFeature(ProductDefinition productdef, MacroDef macrodef, TargetPlatform targetplatform) {
 
     StringMap properties = new StringMap();
     contributeForAll(properties, productdef);
@@ -280,6 +310,30 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
 
   }
 
+  // /**
+  // * Executes the macro for the scope: {@link Scope#ForEachTargetFeature}.
+  // *
+  // * @param productdef
+  // * The productdefinition to be used. Not <code>null</code>.
+  // * @param macrodef
+  // * The macro constituting the scope. Not <code>null</code>.
+  // * @param targetplatform
+  // * The TargetPlatform used to resolve the bundles against. Not <code>null</code>.
+  // */
+  // private void executeForEachTargetFeature(ProductDefinition productdef, MacroDef macrodef,
+  // TargetPlatform targetplatform) {
+  //
+  // StringMap properties = new StringMap();
+  // contributeForAll(properties, productdef);
+  //
+  // String[] featureids = productdef.getFeatureIds();
+  // for (String featureid : featureids) {
+  // contributeForFeature(properties, productdef, featureid);
+  // executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties));
+  // }
+  //
+  // }
+
   /**
    * Executes the macro for the scope: {@link Scope#ForProduct}.
    * 
@@ -287,8 +341,10 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
    *          The productdefinition to be used. Not <code>null</code>.
    * @param macrodef
    *          The macro constituting the scope. Not <code>null</code>.
+   * @param targetplatform
+   *          The TargetPlatform used to resolve the bundles against. Not <code>null</code>.
    */
-  private void executeForProduct(ProductDefinition productdef, MacroDef macrodef) {
+  private void executeForProduct(ProductDefinition productdef, MacroDef macrodef, TargetPlatform targetplatform) {
     StringMap properties = new StringMap();
     contributeForAll(properties, productdef);
     executeMacroInstance(macrodef, new LocalMacroExecutionValuesProvider(properties));
@@ -338,6 +394,7 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
    *          The product definition instance providing the necessary information. Not <code>null</code>.
    */
   private void contributeForAll(StringMap properties, ProductDefinition productdef) {
+
     properties.put(PROP_PRODUCTID, productdef.getId());
     properties.put(PROP_PRODUCTNAME, productdef.getName());
     properties.put(PROP_BASEDONFEATURES, String.valueOf(productdef.isBasedOnFeatures()));
@@ -346,6 +403,19 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
     properties.put(PROP_VERSION, String.valueOf(productdef.getVersion()));
     properties.put(PROP_VMARGS, productdef.getVmArgs(this._os));
     properties.put(PROP_PROGRAMARGS, productdef.getProgramArgs(this._os));
+
+    String configini = productdef.getConfigIni(this._os);
+    if (configini == null) {
+      properties.put(PROP_CONFIGINI, "");
+    } else {
+      int endofprojectname = configini.indexOf('/', 1);
+      String projectname = configini.substring(1, endofprojectname);
+      String childname = configini.substring(endofprojectname + 1);
+      EclipseProject project = getWorkspace().getProject(projectname);
+      File path = project.getChild(childname);
+      properties.put(PROP_CONFIGINI, path.getAbsolutePath());
+    }
+
   }
 
   /**
@@ -372,18 +442,32 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
    * 
    * @param properties
    *          The properties used to be filled with the settings. Not <code>null</code>.
+   * @param references
+   *          The references that will be accessible by the macro. Not <code>null</code>.
    * @param productdef
    *          The product definition instance providing the necessary information. Not <code>null</code>.
    * @param pluginid
    *          The id of the plugin which specific settings shall be applied. Neither <code>null</code> nor empty.
+   * @param targetplatform
+   *          The TargetPlatform used to resolve the bundles against. Not <code>null</code>.
    */
-  private void contributeForPlugin(StringMap properties, ProductDefinition productdef, String pluginid) {
+  private void contributeForPlugin(StringMap properties, Map<String, Object> references, ProductDefinition productdef,
+      String pluginid, TargetPlatform targetplatform) {
     properties.put(PROP_PLUGINID, pluginid);
-    if (getWorkspace().getProject(pluginid) != null) {
+    if (getWorkspace().hasProject(pluginid)) {
+      EclipseProject project = getWorkspace().getProject(pluginid);
+      File location = project.getFolder();
       properties.put(PROP_PLUGINISSOURCE, "true");
+      properties.put(PROP_PLUGINFILE, location.getAbsolutePath());
     } else {
+      BundleDescription bundledesc = targetplatform.getBundleDescription(pluginid);
+      BundleSource bundlesource = (BundleSource) bundledesc.getUserObject();
+      File location = bundlesource.getAsFile();
       properties.put(PROP_PLUGINISSOURCE, "false");
+      properties.put(PROP_PLUGINFILE, location.getAbsolutePath());
+      references.put(PROP_PLUGINFILELIST, FileListHelper.getFileList(location));
     }
+
   }
 
   /**
@@ -393,7 +477,10 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
   private static class LocalMacroExecutionValuesProvider implements MacroExecutionValuesProvider {
 
     /** - */
-    private StringMap _properties;
+    private StringMap           _properties;
+
+    /** - */
+    private Map<String, Object> _references;
 
     /**
      * Initialises this provider using a specified product definition file.
@@ -402,7 +489,20 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
      *          The properties to be set. Not <code>null</code>.
      */
     public LocalMacroExecutionValuesProvider(StringMap properties) {
+      this(properties, null);
+    }
+
+    /**
+     * Initialises this provider using a specified product definition file.
+     * 
+     * @param properties
+     *          The properties to be set. Not <code>null</code>.
+     * @param references
+     *          The references that have to be applied. Maybe <code>null</code>.
+     */
+    public LocalMacroExecutionValuesProvider(StringMap properties, Map<String, Object> references) {
       this._properties = properties;
+      this._references = references;
     }
 
     /**
@@ -410,6 +510,9 @@ public class ExecuteProductTask extends AbstractExecuteProjectTask implements Pd
      */
     public MacroExecutionValues provideMacroExecutionValues(MacroExecutionValues values) {
       values.getProperties().putAll(this._properties);
+      if (this._references != null) {
+        values.getReferences().putAll(this._references);
+      }
       return values;
     }
 
