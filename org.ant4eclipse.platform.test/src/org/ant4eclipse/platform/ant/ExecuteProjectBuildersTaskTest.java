2cdc29115b591c16692e19db934182fec547b60d
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-12-03 12:08:53 +0000

    ~ added a testcase which demonstrates the functionality of the filter which now uses the prefix ISSUE: AE-136

--- org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/ant/ExecuteProjectBuildersTaskTest.java
+++ org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/ant/ExecuteProjectBuildersTaskTest.java
@@ -27,21 +27,35 @@ public class ExecuteProjectBuildersTaskTest extends AbstractWorkspaceBasedBuildF
     new EclipseProjectBuilder("simpleproject_1").createIn(getTestWorkspaceDirectory());
     new EclipseProjectBuilder("simpleproject_2").createIn(getTestWorkspaceDirectory());
 
-    getTestWorkspace().createFile("projectSet.psf", createPsfContent());
+    getTestWorkspace().createFile("projectSet.psf", createPsfContent(false));
 
     expectLog("executeProjectSet", "simpleproject_1simpleproject_2");
   }
 
-  private String createPsfContent() {
-    StringBuffer buffer = new StringBuffer();
+  public void testExecuteProjectBuilders_2() {
 
+    // this variety also makes use of the filter functionality
+    new EclipseProjectBuilder("simpleproject_1").createIn(getTestWorkspaceDirectory());
+    new EclipseProjectBuilder("simpleproject_2.test").createIn(getTestWorkspaceDirectory());
+
+    getTestWorkspace().createFile("projectSet.psf", createPsfContent(true));
+
+    expectLog("executeProjectSetFiltered", "simpleproject_1");
+  }
+
+  private String createPsfContent(boolean test) {
+    StringBuffer buffer = new StringBuffer();
+    String part = test ? ".test" : "";
     buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
     buffer.append("<psf version=\"2.0\">");
     buffer.append("<provider id=\"org.tigris.subversion.subclipse.core.svnnature\">");
     buffer
         .append("<project reference=\"0.9.3,http://svn.javakontor.org/ant4eclipse/trunk/simpleproject_1,simpleproject_1\"/>");
     buffer
-        .append("<project reference=\"0.9.3,http://svn.javakontor.org/ant4eclipse/trunk/simpleproject_2,simpleproject_2\"/>");
+        .append(String
+            .format(
+                "<project reference=\"0.9.3,http://svn.javakontor.org/ant4eclipse/trunk/simpleproject_2%s,simpleproject_2%s\"/>",
+                part, part));
     buffer.append("</provider>");
     buffer.append("</psf>");
 
