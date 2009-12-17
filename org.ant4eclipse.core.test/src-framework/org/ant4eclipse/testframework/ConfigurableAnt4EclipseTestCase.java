3b60e70dac9b2b297bde6e6bdbf681aa02733798
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-12-17 12:40:44 +0000

    - removed the obsolete method 'getResource' ~ when disposing fails a 'fail' will be invoked

--- org.ant4eclipse.core.test/src-framework/org/ant4eclipse/testframework/ConfigurableAnt4EclipseTestCase.java
+++ org.ant4eclipse.core.test/src-framework/org/ant4eclipse/testframework/ConfigurableAnt4EclipseTestCase.java
@@ -15,9 +15,8 @@ import org.ant4eclipse.lib.core.DefaultConfigurator;
 import org.ant4eclipse.lib.core.service.ServiceRegistry;
 import org.ant4eclipse.lib.core.util.StringMap;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
-
-import java.io.InputStream;
 
 /**
  * <p>
@@ -55,36 +54,21 @@ public class ConfigurableAnt4EclipseTestCase {
     } catch (Exception ex) {
       System.err.println("[Ant4EclipseTestCase] Could not reset ServiceRegistry: " + ex);
       ex.printStackTrace();
+      Assert.fail(ex.getMessage());
     }
   }
 
   /**
+   * Provides a set of properties used for the configuration. The supplied set is supposed to be altered and returned.
+   * If a <code>null</code> value is returned the default configuration takes place.
+   * 
    * @param properties
-   * @return
+   *          The current set of properties which is supposed to be altered. Not <code>null</code>.
+   * 
+   * @return The altered properties or <code>null</code>.
    */
   protected StringMap customAnt4EclipseConfiguration(StringMap properties) {
     return null;
   }
 
-  /**
-   * Returns an {@link InputStream} to the given resource that must be specified relative to getClass()
-   * 
-   * @param name
-   * @return
-   */
-  protected InputStream getResource(String name) {
-    String packagename = "/" + getClass().getPackage().getName().replace('.', '/');
-
-    String qualifiedResourceName = packagename + "/" + name;
-
-    InputStream stream = getClass().getResourceAsStream(qualifiedResourceName);
-    if (stream == null) {
-      System.err.println("Resource '" + qualifiedResourceName + "' not found!");
-      throw new RuntimeException("Resource '" + qualifiedResourceName + "' not found!");
-    }
-
-    return stream;
-
-  }
-
-}
+} /* ENDCLASS */
