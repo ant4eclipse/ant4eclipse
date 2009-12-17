448b9f3a184028f8c36e84dd8dd8f5c0deb60f78
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-12-17 12:04:03 +0000

    + added a default implementation for the 'ServiceRegistryConfiguration' interface

---
+++ org.ant4eclipse.lib.core/src/org/ant4eclipse/lib/core/service/DefaultServiceRegistryConfiguration.java
@@ -0,0 +1,64 @@
+/**********************************************************************
+ * Copyright (c) 2005-2009 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.lib.core.service;
+
+import org.ant4eclipse.lib.core.Assure;
+import org.ant4eclipse.lib.core.configuration.Ant4EclipseConfiguration;
+import org.ant4eclipse.lib.core.logging.Ant4EclipseLogger;
+
+/**
+ * <p>
+ * Default implementation of a ServiceRegistryConfiguration
+ * </p>
+ * 
+ * @author Daniel Kasmeroglu (Daniel.Kasmeroglu@Kasisoft.net)
+ */
+public class DefaultServiceRegistryConfiguration implements ServiceRegistryConfiguration {
+
+  private Ant4EclipseLogger        logger;
+
+  private Ant4EclipseConfiguration config;
+
+  /**
+   * Initialises this registry with a valid logger and an apropriate configuration.
+   * 
+   * @param loggerobj
+   *          The logger to be used. Not <code>null</code>.
+   * @param configobj
+   *          The configuration to be used. Not <code>null</code>.
+   */
+  public DefaultServiceRegistryConfiguration(Ant4EclipseLogger loggerobj, Ant4EclipseConfiguration configobj) {
+    Assure.paramNotNull("loggerobj", loggerobj);
+    Assure.paramNotNull("configobj", configobj);
+    this.logger = loggerobj;
+    this.config = configobj;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public void configure(ConfigurationContext context) {
+
+    // 1. add Ant4EclipseLogger
+    context.registerService(this.logger, Ant4EclipseLogger.class.getName());
+
+    // 2. add Ant4EclipseConfiguration
+    context.registerService(this.config, Ant4EclipseConfiguration.class.getName());
+
+    // 3. Configure services from properties
+    PropertiesBasedServiceRegistryConfiguration configuration = new PropertiesBasedServiceRegistryConfiguration(
+        this.config);
+    configuration.configure(context);
+
+  }
+
+} /* ENDCLASS */
