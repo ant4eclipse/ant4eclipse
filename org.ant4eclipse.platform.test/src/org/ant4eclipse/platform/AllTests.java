1a45601d69718dd58c9ab3820ea2f1b2de6b05ee
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-12-17 14:20:40 +0000

    ~ moved these tests from the project 'org.ant4eclipse.platform.test' to 'org.ant4eclipse.lib.platform.test'

--- org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/AllTests.java
+++ org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/AllTests.java
@@ -16,16 +16,12 @@ import org.ant4eclipse.platform.ant.GetProjectDirecoryTest;
 import org.ant4eclipse.platform.ant.HasBuildCommandTest;
 import org.ant4eclipse.platform.ant.HasNatureTest;
 import org.ant4eclipse.platform.ant.delegate.MacroExecutionDelegateTest;
-import org.ant4eclipse.platform.model.resource.role.ProjectRoleIdentifierRegistryTest;
-import org.ant4eclipse.platform.model.team.cvssupport.CvsRootTest;
-import org.ant4eclipse.platform.model.team.projectset.internal.ProjectSetFileParserImplTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { ExecuteProjectBuildersTaskTest.class, HasNatureTest.class, HasBuildCommandTest.class,
-    GetProjectDirecoryTest.class, ProjectRoleIdentifierRegistryTest.class, CvsRootTest.class,
-    ProjectSetFileParserImplTest.class, MacroExecutionDelegateTest.class })
+    GetProjectDirecoryTest.class, MacroExecutionDelegateTest.class })
 public class AllTests {
 }
