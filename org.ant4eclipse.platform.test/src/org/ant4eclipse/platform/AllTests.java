972b9e4fcd1f866a099cfec179aae31d3a909997
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2009-12-17 14:14:57 +0000

    ~ moved the test for the launch configuration from project 'org.ant4eclipse.platform.test' to 'org.ant4eclipse.lib.platform.test'

--- org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/AllTests.java
+++ org.ant4eclipse.platform.test/src/org/ant4eclipse/platform/AllTests.java
@@ -16,7 +16,6 @@ import org.ant4eclipse.platform.ant.GetProjectDirecoryTest;
 import org.ant4eclipse.platform.ant.HasBuildCommandTest;
 import org.ant4eclipse.platform.ant.HasNatureTest;
 import org.ant4eclipse.platform.ant.delegate.MacroExecutionDelegateTest;
-import org.ant4eclipse.platform.internal.model.launcher.LaunchConfigurationReaderImplTest;
 import org.ant4eclipse.platform.model.resource.role.ProjectRoleIdentifierRegistryTest;
 import org.ant4eclipse.platform.model.team.cvssupport.CvsRootTest;
 import org.ant4eclipse.platform.model.team.projectset.internal.ProjectSetFileParserImplTest;
@@ -27,6 +26,6 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses( { ExecuteProjectBuildersTaskTest.class, HasNatureTest.class, HasBuildCommandTest.class,
     GetProjectDirecoryTest.class, ProjectRoleIdentifierRegistryTest.class, CvsRootTest.class,
-    ProjectSetFileParserImplTest.class, MacroExecutionDelegateTest.class, LaunchConfigurationReaderImplTest.class })
+    ProjectSetFileParserImplTest.class, MacroExecutionDelegateTest.class })
 public class AllTests {
 }
