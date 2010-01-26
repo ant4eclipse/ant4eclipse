a8281d5602d5bc4ebb0ea9f17bb38bfe3663b7ef
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 21:51:02 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.lib.jdt.test/src-framework/org/ant4eclipse/jdt/test/builder/JdtProjectBuilder.java
+++ org.ant4eclipse.lib.jdt.test/src-framework/org/ant4eclipse/jdt/test/builder/JdtProjectBuilder.java
@@ -16,13 +16,13 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 
-import org.ant4eclipse.jdt.model.project.JavaProjectRole;
 
 import org.ant4eclipse.platform.test.builder.EclipseProjectBuilder;
 import org.ant4eclipse.platform.test.builder.StringTemplate;
 
 import org.ant4eclipse.lib.core.ClassName;
 import org.ant4eclipse.lib.core.util.Utilities;
+import org.ant4eclipse.lib.jdt.model.project.JavaProjectRole;
 
 import java.io.File;
 import java.io.IOException;
