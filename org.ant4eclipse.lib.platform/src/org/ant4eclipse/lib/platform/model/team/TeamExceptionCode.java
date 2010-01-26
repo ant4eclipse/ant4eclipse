a46f1880590567a8715f0aefb3862c6c7dee6267
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 22:35:36 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.lib.platform/src/org/ant4eclipse/lib/platform/model/team/TeamExceptionCode.java
+++ org.ant4eclipse.lib.platform/src/org/ant4eclipse/lib/platform/model/team/TeamExceptionCode.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
  **********************************************************************/
-package org.ant4eclipse.ant.platform.team;
+package org.ant4eclipse.lib.platform.model.team;
 
 import org.ant4eclipse.lib.core.exception.ExceptionCode;
 
