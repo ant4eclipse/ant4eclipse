ae6be69dc58469ec811e433f8206f1a2500eb9f7
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 00:31:31 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.ant.core/src/org/ant4eclipse/ant/core/Ant4EclipseConfigurator.java
+++ org.ant4eclipse.ant.core/src/org/ant4eclipse/ant/core/Ant4EclipseConfigurator.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
  **********************************************************************/
-package org.ant4eclipse.core;
+package org.ant4eclipse.ant.core;
 
 import org.ant4eclipse.core.ant.AntBasedLogger;
 import org.ant4eclipse.core.configuration.Ant4EclipseConfiguration;
