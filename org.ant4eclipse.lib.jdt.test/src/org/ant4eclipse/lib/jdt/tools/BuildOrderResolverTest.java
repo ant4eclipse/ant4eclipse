40f92ff51eeac82aacb649ac2587a179a4ae690b
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 21:54:53 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.lib.jdt.test/src/org/ant4eclipse/lib/jdt/tools/BuildOrderResolverTest.java
+++ org.ant4eclipse.lib.jdt.test/src/org/ant4eclipse/lib/jdt/tools/BuildOrderResolverTest.java
@@ -14,15 +14,16 @@ package org.ant4eclipse.lib.jdt.tools;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
-import org.ant4eclipse.jdt.test.builder.JdtProjectBuilder;
 
-import org.ant4eclipse.platform.model.resource.EclipseProject;
-import org.ant4eclipse.platform.model.resource.Workspace;
-import org.ant4eclipse.platform.model.resource.workspaceregistry.DefaultEclipseWorkspaceDefinition;
-import org.ant4eclipse.platform.model.resource.workspaceregistry.WorkspaceRegistry;
-import org.ant4eclipse.platform.tools.BuildOrderResolver;
 
+import org.ant4eclipse.lib.core.service.ServiceRegistry;
+import org.ant4eclipse.lib.platform.model.resource.EclipseProject;
+import org.ant4eclipse.lib.platform.model.resource.Workspace;
+import org.ant4eclipse.lib.platform.model.resource.workspaceregistry.DefaultEclipseWorkspaceDefinition;
+import org.ant4eclipse.lib.platform.model.resource.workspaceregistry.WorkspaceRegistry;
+import org.ant4eclipse.lib.platform.tools.BuildOrderResolver;
 import org.ant4eclipse.testframework.ConfigurableAnt4EclipseTestCase;
+import org.ant4eclipse.testframework.JdtProjectBuilder;
 import org.ant4eclipse.testframework.TestDirectory;
 import org.junit.Test;
 
@@ -56,7 +57,7 @@ public class BuildOrderResolverTest extends ConfigurableAnt4EclipseTestCase {
 
   @Test
   public void buildOrder() {
-    WorkspaceRegistry workspaceRegistry = WorkspaceRegistry.Helper.getRegistry();
+    WorkspaceRegistry workspaceRegistry = ServiceRegistry.instance().getService(WorkspaceRegistry.class);
     Workspace workspace = workspaceRegistry.registerWorkspace(this._testWorkspace.getRootDir().getAbsolutePath(),
         new DefaultEclipseWorkspaceDefinition(this._testWorkspace.getRootDir()));
 
