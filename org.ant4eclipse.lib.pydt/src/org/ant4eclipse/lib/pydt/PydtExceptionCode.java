2ea8e37d2de4561238123697ea648fc0d00c9c79
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 00:42:12 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.lib.pydt/src/org/ant4eclipse/lib/pydt/PydtExceptionCode.java
+++ org.ant4eclipse.lib.pydt/src/org/ant4eclipse/lib/pydt/PydtExceptionCode.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
  **********************************************************************/
-package org.ant4eclipse.pydt;
+package org.ant4eclipse.lib.pydt;
 
 import org.ant4eclipse.core.exception.ExceptionCode;
 import org.ant4eclipse.core.nls.NLS;
