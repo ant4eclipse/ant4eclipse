03364018fb2d56fcfc141821ce588c53c9f83efb
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-01-26 00:10:40 +0000

    + #AE-81: merge ISSUE: AE-81

--- org.ant4eclipse.lib.jdt/src/org/ant4eclipse/lib/jdt/JdtExceptionCode.java
+++ org.ant4eclipse.lib.jdt/src/org/ant4eclipse/lib/jdt/JdtExceptionCode.java
@@ -9,7 +9,7 @@
  * Contributors:
  *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
  **********************************************************************/
-package org.ant4eclipse.jdt;
+package org.ant4eclipse.lib.jdt;
 
 import org.ant4eclipse.core.exception.ExceptionCode;
 import org.ant4eclipse.core.nls.NLS;
