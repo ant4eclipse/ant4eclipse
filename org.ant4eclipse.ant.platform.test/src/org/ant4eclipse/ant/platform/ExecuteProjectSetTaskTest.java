25f498efacc13bf66481125c276e38905dc59122
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-02-04 21:54:03 +0000

    ~ creation of initial testcases for the executeProjectSet task ISSUE: AE-81

--- org.ant4eclipse.ant.platform.test/src/org/ant4eclipse/ant/platform/ExecuteProjectSetTaskTest.java
+++ org.ant4eclipse.ant.platform.test/src/org/ant4eclipse/ant/platform/ExecuteProjectSetTaskTest.java
@@ -11,6 +11,95 @@
  **********************************************************************/
 package org.ant4eclipse.ant.platform;
 
-public class ExecuteProjectSetTaskTest {
+import org.ant4eclipse.platform.test.AbstractWorkspaceBasedBuildFileTest;
+
+import org.ant4eclipse.testframework.EclipseProjectBuilder;
+
+import java.io.File;
+
+public class ExecuteProjectSetTaskTest extends AbstractWorkspaceBasedBuildFileTest {
+
+  private static final String[] PROJECTNAMES_VALID = new String[] { "org.ant4eclipse.lib.core",
+      "org.ant4eclipse.lib.core.test", "org.ant4eclipse.external", "org.ant4eclipse.lib.jdt",
+      "org.ant4eclipse.lib.jdt.ecj", "org.ant4eclipse.lib.jdt.test", "org.ant4eclipse.lib.platform",
+      "org.ant4eclipse.lib.platform.test", "org.ant4eclipse.lib.pde", "org.ant4eclipse.lib.pde.test",
+      "org.ant4eclipse.lib.pydt", "org.ant4eclipse.lib.pydt.test" };
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setUp() throws Exception {
+
+    super.setUp();
+
+    boolean first = true;
+    for (String projectname : PROJECTNAMES_VALID) {
+      EclipseProjectBuilder builder = new EclipseProjectBuilder(projectname);
+      if (first) {
+        File input = new File("input");
+        File file1 = new File(input, "valid-projectset.psf");
+        File file2 = new File(input, "invalid-projectset.psf");
+        builder.withResource(file1);
+        builder.withResource(file2);
+        first = false;
+      }
+      builder.createIn(getTestWorkspaceDirectory());
+    }
+
+    setupBuildFile("executeProjectSet.xml");
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void tearDown() throws Exception {
+  }
+
+  public void testExecuteProjectSet() {
+    StringBuffer expected = new StringBuffer();
+    for (String projectname : PROJECTNAMES_VALID) {
+      expected.append(String.format("%s%s%s~", getTestWorkspaceDirectory().getAbsolutePath(), File.separator,
+          projectname));
+    }
+    expectLog("executeProjectSet", expected.toString());
+  }
+
+  public void testExecuteProjectSetFiltered() {
+    StringBuffer expected = new StringBuffer();
+    for (String projectname : PROJECTNAMES_VALID) {
+      if (!projectname.endsWith(".test")) {
+        expected.append(String.format("%s%s%s~", getTestWorkspaceDirectory().getAbsolutePath(), File.separator,
+            projectname));
+      }
+    }
+    expectLog("executeProjectSetFiltered", expected.toString());
+  }
+
+  public void testExecuteInvalidProjectSet() {
+    StringBuffer expected = new StringBuffer();
+    for (String projectname : PROJECTNAMES_VALID) {
+      if (projectname.indexOf(".jdt") == -1) {
+        expected.append(String.format("%s%s%s~", getTestWorkspaceDirectory().getAbsolutePath(), File.separator,
+            projectname));
+      }
+    }
+    expectLog("executeInvalidProjectSet", expected.toString());
+  }
+
+  public void testExecuteInvalidProjectSetFiltered() {
+    StringBuffer expected = new StringBuffer();
+    for (String projectname : PROJECTNAMES_VALID) {
+      if (!projectname.endsWith(".test")) {
+        if (projectname.indexOf(".jdt") == -1) {
+          expected.append(String.format("%s%s%s~", getTestWorkspaceDirectory().getAbsolutePath(), File.separator,
+              projectname));
+        }
+      }
+    }
+    expectLog("executeInvalidProjectSetFiltered", expected.toString());
+  }
 
 }
