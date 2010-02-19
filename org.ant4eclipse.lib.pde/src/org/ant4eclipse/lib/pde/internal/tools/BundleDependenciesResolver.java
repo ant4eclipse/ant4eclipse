00e4e6053860bec4880a67ec815b9e0631e5f345
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-02-19 11:36:16 +0000

    + added debug message in getReexportedBundles

--- org.ant4eclipse.lib.pde/src/org/ant4eclipse/lib/pde/internal/tools/BundleDependenciesResolver.java
+++ org.ant4eclipse.lib.pde/src/org/ant4eclipse/lib/pde/internal/tools/BundleDependenciesResolver.java
@@ -12,6 +12,7 @@
 package org.ant4eclipse.lib.pde.internal.tools;
 
 import org.ant4eclipse.lib.core.Assure;
+import org.ant4eclipse.lib.core.logging.A4ELogging;
 import org.ant4eclipse.lib.core.osgi.BundleLayoutResolver;
 import org.ant4eclipse.lib.core.osgi.ExplodedBundleLayoutResolver;
 import org.ant4eclipse.lib.core.osgi.JaredBundleLayoutResolver;
@@ -242,6 +243,11 @@ public class BundleDependenciesResolver {
    */
   private BundleDescription[] getReexportedBundles(BundleDescription bundleDescription) {
     Assure.notNull("bundleDescription", bundleDescription);
+
+    if (A4ELogging.isDebuggingEnabled()) {
+      A4ELogging.debug("Resolving reexported bundles for bundle '%s'", TargetPlatformImpl
+          .getBundleInfo(bundleDescription));
+    }
 
     if (!bundleDescription.isResolved()) {
       String resolverErrors = TargetPlatformImpl.dumpResolverErrors(bundleDescription, true);
