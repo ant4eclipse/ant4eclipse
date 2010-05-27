1146dce01eb7cc747b9991c591ac550b9ac78548
danielkasmeroglu <danielkasmeroglu@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-05-27 22:12:02 +0000

    ~ improved the 'JdtCompilerTask' so an enhancement allows to change the classpath used to access the compiler adapter

--- org.ant4eclipse.ant.jdt/src/org/ant4eclipse/ant/jdt/JdtCompilerTask.java
+++ org.ant4eclipse.ant.jdt/src/org/ant4eclipse/ant/jdt/JdtCompilerTask.java
@@ -13,8 +13,17 @@ package org.ant4eclipse.ant.jdt;
 
 import org.ant4eclipse.ant.jdt.ecj.EcjCompilerAdapter;
 import org.ant4eclipse.lib.core.logging.A4ELogging;
+import org.apache.tools.ant.AntClassLoader;
+import org.apache.tools.ant.AntTypeDefinition;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.ComponentHelper;
 import org.apache.tools.ant.taskdefs.Javac;
+import org.apache.tools.ant.types.Path;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.regex.Pattern;
 
 /**
  * <p>
@@ -27,7 +36,32 @@ import org.apache.tools.ant.taskdefs.Javac;
  */
 public class JdtCompilerTask extends Javac {
 
-  private static final String MSG_INVALID_ATTRIBUTE = "The attribute 'compiler' for the task '%s' is not allowed to be used.";
+  private static final String MSG_SKIPPING_ENHANCEMENT         = "Skipping resource '%s' for the enhancement as it's type couldn't be determined !";
+
+  private static final String MSG_FAILED_TO_ACCESS_CLASSLOADER = "The typedefition for '%s' doesn't provide a usable ClassLoader (%s) !";
+
+  private static final String MSG_INVALID_ATTRIBUTE            = "The attribute 'compiler' for the task '%s' is not allowed to be used.";
+
+  private static final String MSG_ENHANCEMENT_FAILED           = "The task '%s' tried to enhance but failed for the following reason: '%s'. A minimum Ant version 1.8 is required !";
+
+  private boolean             enhance;
+
+  public JdtCompilerTask() {
+    super();
+    this.enhance = false;
+  }
+
+  /**
+   * If set to <code>true</code> and under the precondition that ant 1.8 is used the compiler adapter can be used in an
+   * environment where the antlib loading mechanism isn't used.
+   * 
+   * @param newenhance
+   *          <code>true</code> <=> Use the ant 1.8 mechanism to provide a classpath for the compiler adapter
+   *          instantiation.
+   */
+  public void setEnhance(boolean newenhance) {
+    this.enhance = newenhance;
+  }
 
   /**
    * {@inheritDoc}
@@ -38,12 +72,67 @@ public class JdtCompilerTask extends Javac {
   }
 
   /**
+   * Enhances the classpath used for the compiler lookup. This is only possible since ant 1.8 so the enhancement won't
+   * have any effect on earlier versions.
+   */
+  private void enhanceCompilerClasspath() {
+
+    try {
+
+      // get the Path instance first
+      Method method = getClass().getMethod("createCompilerClasspath");
+      Path path = (Path) method.invoke(this);
+
+      // now we need to access our classpath which has been provided using the taskdef element
+      ComponentHelper helper = ComponentHelper.getComponentHelper(getProject());
+      AntTypeDefinition typedef = helper.getDefinition(getTaskName());
+      if (typedef.getClassLoader() instanceof AntClassLoader) {
+
+        AntClassLoader cl = (AntClassLoader) typedef.getClassLoader();
+
+        // get the classpath and split it
+        String classpath = cl.getClasspath();
+        String pathsep = getProject().getProperty("path.separator");
+        String[] parts = classpath.split(Pattern.quote(pathsep));
+        for (String p : parts) {
+
+          // add the classpath entry for the classpath used to lookup the compiler
+          File file = new File(p);
+          if (file.isDirectory()) {
+            path.createPathElement().setPath(file.getAbsolutePath());
+          } else if (file.isFile()) {
+            path.createPathElement().setLocation(file);
+          } else {
+            A4ELogging.warn(MSG_SKIPPING_ENHANCEMENT, file.getAbsolutePath());
+          }
+
+        }
+
+      } else {
+        A4ELogging
+            .error(MSG_FAILED_TO_ACCESS_CLASSLOADER, getTaskName(), typedef.getClassLoader().getClass().getName());
+      }
+
+    } catch (NoSuchMethodException ex) {
+      A4ELogging.error(MSG_ENHANCEMENT_FAILED, getTaskName(), ex.getMessage());
+    } catch (IllegalArgumentException ex) {
+      A4ELogging.error(MSG_ENHANCEMENT_FAILED, getTaskName(), ex.getMessage());
+    } catch (IllegalAccessException ex) {
+      A4ELogging.error(MSG_ENHANCEMENT_FAILED, getTaskName(), ex.getMessage());
+    } catch (InvocationTargetException ex) {
+      A4ELogging.error(MSG_ENHANCEMENT_FAILED, getTaskName(), ex.getMessage());
+    }
+  }
+
+  /**
    * {@inheritDoc}
    */
   @Override
   public void execute() throws BuildException {
+    if (this.enhance) {
+      enhanceCompilerClasspath();
+    }
     super.setCompiler(EcjCompilerAdapter.class.getName());
     super.execute();
   }
-
 } /* ENDCLASS */
