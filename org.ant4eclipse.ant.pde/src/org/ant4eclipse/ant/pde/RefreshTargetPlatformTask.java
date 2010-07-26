99234532bc0676720f2cf0a038ef10eb2c8f897c
gerdwuetherich <gerdwuetherich@d1cb4da4-e496-4248-86fa-ddd66e329890>
2010-07-26 19:03:08 +0000

    OPEN - issue AE-231: Support for 'refreshable' target platform definitions http://www.javakontor.org:8080/jira/browse/AE-231 ISSUE: AE-231

---
+++ org.ant4eclipse.ant.pde/src/org/ant4eclipse/ant/pde/RefreshTargetPlatformTask.java
@@ -0,0 +1,51 @@
+/**********************************************************************
+ * Copyright (c) 2005-2010 ant4eclipse project team.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Nils Hartmann, Daniel Kasmeroglu, Gerd Wuetherich
+ **********************************************************************/
+package org.ant4eclipse.ant.pde;
+
+import org.ant4eclipse.ant.core.AbstractAnt4EclipseTask;
+import org.ant4eclipse.lib.core.service.ServiceRegistryAccess;
+import org.ant4eclipse.lib.pde.tools.TargetPlatformRegistry;
+
+/**
+ * <p>
+ * Implements an ant task that allows the user to refresh target platforms. That can be useful if one would use the PDE
+ * artifacts from one build step in a second build step (e.g. built plug-ins to build features or prodcuts).
+ * </p>
+ * 
+ * @author Gerd W&uuml;therich (gerd@gerd-wuetherich.de)
+ */
+public class RefreshTargetPlatformTask extends AbstractAnt4EclipseTask {
+
+  /**
+   * <p>
+   * Creates a new instance of type {@link RefreshTargetPlatformTask}.
+   * </p>
+   */
+  public RefreshTargetPlatformTask() {
+    super();
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void doExecute() {
+
+    // get the target platform registry
+    TargetPlatformRegistry targetPlatformRegistry = ServiceRegistryAccess.instance().getService(
+        TargetPlatformRegistry.class);
+
+    // refresh all target platforms
+    targetPlatformRegistry.refreshAll();
+
+  }
+}
