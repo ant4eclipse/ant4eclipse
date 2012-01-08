bbdfcb2a5c308649faa297485e472eb056b3a24e
Daniel Kasmeroglu <Daniel.Kasmeroglu@kasisoft.net>
2012-01-08 16:56:55 +0100

    ~~

--- org.ant4eclipse.lib.core/src/org/ant4eclipse/lib/core/Assure.java
+++ org.ant4eclipse.lib.core/src/org/ant4eclipse/lib/core/Assure.java
@@ -82,11 +82,11 @@ public class Assure {
    *          The object that has to be tested.
    */
   public static final void nonEmpty( String parametername, byte[] object ) {
-    notNull( parametername, object );
-    if( object.length == 0 ) {
-      throw new Ant4EclipseException( CoreExceptionCode.PRECONDITION_VIOLATION, String.format( MSG_NOTEMPTY,
-          parametername ) );
-    }
+//    notNull( parametername, object );
+//    if( object.length == 0 ) {
+//      throw new Ant4EclipseException( CoreExceptionCode.PRECONDITION_VIOLATION, String.format( MSG_NOTEMPTY,
+//          parametername ) );
+//    }
   }
 
   /**
