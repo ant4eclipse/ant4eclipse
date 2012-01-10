1f21c0b78ca4dad92e19adec821984305bba9f04
Daniel Kasmeroglu <Daniel.Kasmeroglu@kasisoft.net>
2012-01-10 22:27:25 +0100

    ~

--- org.ant4eclipse.lib.core.test/src/org/ant4eclipse/lib/core/util/UtilitiesTest.java
+++ org.ant4eclipse.lib.core.test/src/org/ant4eclipse/lib/core/util/UtilitiesTest.java
@@ -151,34 +151,34 @@ public class UtilitiesTest extends ConfigurableAnt4EclipseTestCase {
     String text = "My Message";
     byte[] input = text.getBytes();
 
-    System.err.println("#s0");
-    ByteArrayInputStream instream1 = new ByteArrayInputStream( input );
-    ByteArrayOutputStream outstream1 = new ByteArrayOutputStream();
-    System.err.println("#s1");
-    try {
-      System.err.println("#s2");
-      Utilities.copy( instream1, outstream1, new byte[0] );
-      System.err.println("#s3");
-    } catch( Ant4EclipseException ex ) {
-      System.err.println("#s4");
-      Assert.assertEquals( CoreExceptionCode.PRECONDITION_VIOLATION, ex.getExceptionCode() );
-    } finally {
-      System.err.println("#s5");
-      Utilities.close( (Closeable) instream1 );
-      Utilities.close( (Closeable) outstream1 );
-    }
-
-//    ByteArrayInputStream instream2 = new ByteArrayInputStream( input );
-//    ByteArrayOutputStream outstream2 = new ByteArrayOutputStream();
-
+//    System.err.println("#s0");
+//    ByteArrayInputStream instream1 = new ByteArrayInputStream( input );
+//    ByteArrayOutputStream outstream1 = new ByteArrayOutputStream();
+//    System.err.println("#s1");
 //    try {
-//      Utilities.copy( instream2, outstream2, new byte[10] );
+//      System.err.println("#s2");
+//      Utilities.copy( instream1, outstream1, new byte[0] );
+//      System.err.println("#s3");
+//    } catch( Ant4EclipseException ex ) {
+//      System.err.println("#s4");
+//      Assert.assertEquals( CoreExceptionCode.PRECONDITION_VIOLATION, ex.getExceptionCode() );
 //    } finally {
-//      Utilities.close( (Closeable) instream2 );
-//      Utilities.close( (Closeable) outstream2 );
+//      System.err.println("#s5");
+//      Utilities.close( (Closeable) instream1 );
+//      Utilities.close( (Closeable) outstream1 );
 //    }
 
-//    Assert.assertEquals( text, new String( outstream2.toByteArray() ) );
+    ByteArrayInputStream instream2 = new ByteArrayInputStream( input );
+    ByteArrayOutputStream outstream2 = new ByteArrayOutputStream();
+
+    try {
+      Utilities.copy( instream2, outstream2, new byte[10] );
+    } finally {
+      Utilities.close( (Closeable) instream2 );
+      Utilities.close( (Closeable) outstream2 );
+    }
+
+    Assert.assertEquals( text, new String( outstream2.toByteArray() ) );
 
   }
 
