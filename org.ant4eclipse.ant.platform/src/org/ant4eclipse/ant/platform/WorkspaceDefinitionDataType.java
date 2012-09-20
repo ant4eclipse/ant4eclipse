25232d836870dee5944df731a37b0002f4004086
laeubi <laeubi@laeubi-soft.de>
2012-09-20 10:59:38 +0200

    AE-269 Allow workspaceDefinition to scan sub-folders  Added new property to scan subfolders, output a warning if workscpace is empty

--- org.ant4eclipse.ant.platform/src/org/ant4eclipse/ant/platform/WorkspaceDefinitionDataType.java
+++ org.ant4eclipse.ant.platform/src/org/ant4eclipse/ant/platform/WorkspaceDefinitionDataType.java
@@ -1,5 +1,10 @@
 package org.ant4eclipse.ant.platform;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ant4eclipse.ant.core.AbstractAnt4EclipseDataType;
 import org.ant4eclipse.lib.core.logging.A4ELogging;
 import org.ant4eclipse.lib.core.service.ServiceRegistryAccess;
@@ -7,12 +12,9 @@ import org.ant4eclipse.lib.platform.internal.model.resource.workspaceregistry.Pr
 import org.ant4eclipse.lib.platform.model.resource.workspaceregistry.FilesetWorkspaceDefinition;
 import org.ant4eclipse.lib.platform.model.resource.workspaceregistry.WorkspaceRegistry;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.DirSet;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 
@@ -33,6 +35,11 @@ public class WorkspaceDefinitionDataType extends AbstractAnt4EclipseDataType {
   private List<DirSet> _dirSet;
 
   /**
+   * indicates if this definition scanns directories recursive
+   */
+  private boolean      recursive;
+
+  /**
    * <p>
    * Creates a new instance of type {@link WorkspaceDefinitionDataType}.
    * </p>
@@ -41,7 +48,6 @@ public class WorkspaceDefinitionDataType extends AbstractAnt4EclipseDataType {
    */
   public WorkspaceDefinitionDataType(Project project) {
     super(project);
-
     //
     this._dirSet = new ArrayList<DirSet>();
   }
@@ -52,6 +58,14 @@ public class WorkspaceDefinitionDataType extends AbstractAnt4EclipseDataType {
     }
 
     this._id = id;
+  }
+
+  /**
+   * @param recursive
+   *          the new value for recursive
+   */
+  public void setRecursive(boolean recursive) {
+    this.recursive = recursive;
   }
 
   /**
@@ -76,47 +90,97 @@ public class WorkspaceDefinitionDataType extends AbstractAnt4EclipseDataType {
     if (this._id == null) {
       throw new BuildException("Workspace registration requires the identifier property to be set!");
     }
-
     // create a workspace from the filesets
     List<File> projectDirectories = new ArrayList<File>();
     for (DirSet fs : this._dirSet) {
-      File fsBase = fs.getDirectoryScanner().getBasedir();
-      for (String d : fs.getDirectoryScanner().getIncludedDirectories()) {
+      DirectoryScanner directoryScanner = fs.getDirectoryScanner();
+      File fsBase = directoryScanner.getBasedir();
+      for (String d : directoryScanner.getIncludedDirectories()) {
         File file = new File(fsBase, d);
-
-        // make sure it exists
-        if (!file.exists()) {
-          A4ELogging.debug("File " + file + " does not exist and will be ignored.");
-          continue;
-        }
-
-        // make sure its a directory
-        if (!file.isDirectory()) {
-          A4ELogging.debug("File " + file + " is not a directory and will be ignored.");
-          continue;
-        }
-
-        // and it should have a .project file
-        if (!ProjectFileParser.isProjectDirectory(file)) {
-          A4ELogging.debug("File " + file + " is not an eclipse project directory and will be ignored.");
-          continue;
-        }
-
-        projectDirectories.add(file);
+        addDirectory(projectDirectories, file);
       }
     }
-
-    // if (A4ELogging.isDebuggingEnabled()) {
-    // A4ELogging.debug("Workspace " + this.workspaceDirectory + " will be registered with " +
-    // projectDirectories.size()
-    // + " projects:");
-    // for (File f : projectDirectories) {
-    // A4ELogging.debug("\t" + f.getAbsolutePath());
-    // }
-    // }
-
+    if (projectDirectories.isEmpty()) {
+      A4ELogging.warn("No directories found for workspace %s", this._id);
+    }
     WorkspaceRegistry registry = ServiceRegistryAccess.instance().getService(WorkspaceRegistry.class);
-    registry.registerWorkspace(this._id, new FilesetWorkspaceDefinition(projectDirectories
-        .toArray(new File[projectDirectories.size()])));
+    registry.registerWorkspace(this._id,
+        new FilesetWorkspaceDefinition(projectDirectories.toArray(new File[projectDirectories.size()])));
+  }
+
+  /**
+   * Adds an directory if it is valid and is a eclipse project to the set of project directories
+   * 
+   * @param projectDirectories
+   * @param file
+   */
+  private void addDirectory(List<File> projectDirectories, File file) {
+    if (!isValidDirectory(file)) {
+      return;
+    }
+    // and it should have a .project file
+    if (!isEclipseProject(file)) {
+      // Do we use recursive scanning?
+      if (this.recursive) {
+        A4ELogging.debug("Scann " + file + " for subdirectories of eclipse projects.");
+        File[] subFolders = file.listFiles(new FileFilter() {
+
+          public boolean accept(File pathname) {
+            return pathname.isDirectory();
+          }
+        });
+        // Any subfolders to scan?
+        if (subFolders.length > 0) {
+          for (File subFolder : subFolders) {
+            addDirectory(projectDirectories, subFolder);
+          }
+          return;
+        }
+      }
+      A4ELogging.debug("File " + file + " is not an eclipse project directory and will be ignored.");
+      return;
+    }
+    // Everything is fine, we add this directory!
+    A4ELogging.info("Add project for location %s", file);
+    projectDirectories.add(file);
+  }
+
+  /**
+   * Checks if this file is a valid eclipse project.
+   * 
+   * @param file
+   * @return
+   */
+  protected boolean isEclipseProject(File file) {
+    return ProjectFileParser.isProjectDirectory(file);
+  }
+
+  /**
+   * Chechs if the given argument is a valid directory in terms of file system view
+   * 
+   * @param file
+   * @return
+   */
+  protected boolean isValidDirectory(File file) {
+    // make sure it exists
+    if (!file.exists()) {
+      A4ELogging.debug("File " + file + " does not exist and will be ignored.");
+      return false;
+    }
+    // make sure its a directory
+    if (!file.isDirectory()) {
+      A4ELogging.debug("File " + file + " is not a directory and will be ignored.");
+      return false;
+    }
+    // and not a "excluded" one
+    String[] excludes = DirectoryScanner.getDefaultExcludes();
+    String fileName = file.toString();
+    for (String exclude : excludes) {
+      if (DirectoryScanner.match(exclude, fileName)) {
+        A4ELogging.debug("File " + file + " is an excluded directory and will be ignored.");
+        return false;
+      }
+    }
+    return true;
   }
 }
